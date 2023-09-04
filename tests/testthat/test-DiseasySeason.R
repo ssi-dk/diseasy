7d7f2b86a5839afbeb583ec6caf2de0518ec4576
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-09-04 13:54:10 +0200

    feat: add DiseasySeason module

---
+++ tests/testthat/test-DiseasySeason.R
@@ -0,0 +1,244 @@
+test_that("initialize works", {
+
+  # Creating an empty module
+  s <- DiseasySeason$new()
+  expect_null(s$reference_date)
+  rm(s)
+
+  # Perturbations of the initializer inputs
+  s <- DiseasySeason$new(reference_date = as.Date("2022-01-05"))
+  expect_identical(s$reference_date, as.Date("2022-01-05"))
+  rm(s)
+
+  # A empty module should have season functions that are constant
+  s <- DiseasySeason$new()
+  expect_identical(s$model_t(0),   1)
+  expect_identical(s$model_t(100), 1)
+  rm(s)
+
+})
+
+
+test_that("set_reference_date works", {
+
+  # Creating an empty module
+  s <- DiseasySeason$new()
+
+  # Testing malformed inputs
+  s$set_reference_date(as.Date("2022-01-01"))
+  expect_identical(s$reference_date, as.Date("2022-01-01"))
+
+  # Testing malformed inputs
+  expect_error(s$set_reference_date("2022-01-02"),
+               class = "simpleError", regexp = "Must be of class 'Date'")
+  expect_identical(s$reference_date, as.Date("2022-01-01"))
+
+  expect_error(s$set_reference_date(as.Date(NA)),
+               class = "simpleError", regexp = "Contains missing values")
+  expect_identical(s$reference_date, as.Date("2022-01-01"))
+  rm(s)
+
+})
+
+
+test_that("use_constant_season works", {
+
+  # Creating an empty module
+  s <- DiseasySeason$new(reference_date = as.Date("2022-01-01"))
+
+  # Default scale
+  s$use_constant_season()
+
+  expect_identical(s$model_t(0), 1)
+  expect_identical(s$model_t(1), 1)
+  expect_identical(s$model_date(as.Date("2022-01-01")), 1)
+  expect_identical(s$model_date(as.Date("2022-01-02")), 1)
+
+  # Malformed inputs
+  s$set_reference_date(as.Date("2022-02-01"))
+  expect_identical(s$model_t(0), 1)
+  expect_identical(s$model_t(1), 1)
+  expect_identical(s$model_date(as.Date("2022-01-01")), 1)
+  expect_identical(s$model_date(as.Date("2022-01-02")), 1)
+
+  rm(s)
+
+})
+
+
+test_that("use_cosine_season works", {
+
+  # Creating an empty module
+  s <- DiseasySeason$new(reference_date = as.Date("2022-01-01"))
+
+  # Default scale
+  s$use_cosine_season()
+
+  expect_identical(s$model_t(0), 1)
+  expect_false(s$model_t(1) == 1)
+
+  expect_identical(s$model_date(as.Date("2022-01-01")), 1)
+  expect_false(s$model_date(as.Date("2022-01-02")) == 1)
+
+
+  # Custom scale
+  s$use_cosine_season(scale = 0.35)
+
+  expect_identical(s$model_t(0), 1)
+  expect_false(s$model_t(1) == 1)
+
+  expect_identical(s$model_date(as.Date("2022-01-01")), 1)
+  expect_false(s$model_date(as.Date("2022-01-02")) == 1)
+
+  # Malformed inputs
+  expect_error(s$use_cosine_season(scale = NA), "May not be NA")
+  expect_error(s$use_cosine_season(scale = -1), "not >= 0")
+  expect_error(s$use_cosine_season(scale = 2), "not <= 1")
+
+  expect_error(s$use_cosine_season(peak = NA), "May not be NA")
+  expect_error(s$use_cosine_season(peak = -1), "not >= 0")
+  expect_error(s$use_cosine_season(peak = 500), "not <= 365")
+
+  rm(s)
+
+})
+
+
+test_that("use_covid_season_v1 works", {
+
+  # Creating an empty module
+  s <- DiseasySeason$new(reference_date = as.Date("2022-01-01"))
+
+  # Default scale
+  s$use_covid_season_v1()
+
+  expect_identical(s$model_t(0), 1)
+  expect_false(s$model_t(1) == 1)
+
+  expect_identical(s$model_date(as.Date("2022-01-01")), 1)
+  expect_false(s$model_date(as.Date("2022-01-02")) == 1)
+
+
+  # Custom scale
+  s$use_covid_season_v1(scale = 0.35)
+
+  expect_identical(s$model_t(0), 1)
+  expect_false(s$model_t(1) == 1)
+
+  expect_identical(s$model_date(as.Date("2022-01-01")), 1)
+  expect_false(s$model_date(as.Date("2022-01-02")) == 1)
+
+
+  # Malformed scale
+  expect_error(s$use_covid_season_v1(scale = 0.99), "not <= 0.69")
+
+  rm(s)
+
+})
+
+# Disabled for now. Uses implementation specific code.
+# Will be re-enabled once DiseasySeason is rewritten to use DiseasyObservable module
+# nolint start commented_code_linter
+# test_that("use_covid_season_v2 works", {
+#   conn <- test_conn()
+#
+#   # Creating an empty module
+#   s <- DiseasySeason$new(reference_date = as.Date("2022-01-01"), conn = conn)
+#
+#   # Default scale
+#   s$use_covid_season_v2()
+#
+#   expect_identical(s$model_t(0), 1)
+#   expect_false(s$model_t(1) == 1)
+#
+#   expect_identical(s$model_date(as.Date("2022-01-01")), 1)
+#   expect_false(s$model_date(as.Date("2022-01-02")) == 1)
+#
+#
+#   # Custom scale
+#   s$use_covid_season_v2(scale = 0.35)
+#
+#   expect_identical(s$model_t(0), 1)
+#   expect_false(s$model_t(1) == 1)
+#
+#   expect_identical(s$model_date(as.Date("2022-01-01")), 1)
+#   expect_false(s$model_date(as.Date("2022-01-02")) == 1)
+#
+#
+#   # Malformed scale
+#   expect_error(s$use_covid_season_v2(scale = 0.99), "not <= 0.95")
+#
+#   rm(s)
+#   SCDB::close_connection(conn)
+# })
+# nolint end commented_code_linter
+
+
+test_that("set_reference_date (with model set) works", {
+
+  # Creating an empty module
+  s <- DiseasySeason$new(reference_date = as.Date("2022-01-01"))
+
+  # Default scale
+  s$use_covid_season_v1()
+
+  tmp1 <- s$model_t(0)
+
+  expect_identical(s$model_t(0), 1)
+  expect_false(s$model_t(1) == 1)
+
+  expect_identical(s$model_date(as.Date("2022-01-01")), 1)
+  expect_false(s$model_date(as.Date("2022-01-02")) == 1)
+
+  s$set_reference_date(as.Date("2022-02-01"))
+  expect_identical(s$model_date(as.Date("2022-02-01")), 1)
+  expect_false(s$model_date(as.Date("2022-01-01")) == 1)
+
+  # Custom scale
+  s$use_covid_season_v1(scale = 0.35)
+
+  tmp2 <- s$model_t(1)
+
+  s$set_reference_date(as.Date("2022-01-01"))
+  s$set_reference_date(as.Date("2022-02-01"))
+
+  expect_identical(s$model_t(1), tmp2)
+  expect_false(s$model_t(1) == tmp1)
+
+  rm(s)
+
+})
+
+
+test_that("set_scale works", {
+
+  # Creating an empty module
+  s <- DiseasySeason$new(reference_date = as.Date("2022-01-01"))
+
+  # Default scale
+  s$use_cosine_season(scale = 0.35)
+
+  s_100 <- s$model_t(100)
+  expect_identical(s$model_t(0), 1)
+  expect_false(s_100 == 1)
+
+  # New model with different scale
+  s$use_cosine_season(scale = 0.5)
+  expect_identical(s$model_t(0), 1)
+  expect_false(s$model_t(100) == 1)
+  expect_true(s$model_t(100) < s_100)
+
+  # Setting scale back to 0.35
+  s$set_scale(scale = 0.35)
+  expect_identical(s$model_t(0), 1)
+  expect_identical(s$model_t(100), s_100)
+
+  # Malformed scale
+  s$use_constant_season()
+  expect_identical(s$model_t(0), 1)
+  expect_identical(s$model_t(1), 1)
+  expect_error(s$set_scale(0.5), regexp = "does not use scale argument")
+
+  rm(s)
+
+})
