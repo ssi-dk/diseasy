82f57b90118e971f75d80e65f5c0281e2d6d264f
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-09-04 11:05:31 +0200

    docs: add extra documentation templates

--- R/0_documentation.R
+++ R/0_documentation.R
@@ -1,7 +1,52 @@
+rd_aggregation <- function(type = "param") {
+  checkmate::assert_choice(type, c("param", "field"))
+  paste("aggregation (`list`(`quosures`))\\cr",
+        "Default NULL.",
+        "If given, expressions in aggregation evaluated to give the aggregation level.",
+        ifelse(type == "field", " Read only.", ""))
+}
+
+
 rd_case_definition <- function(type = "param") {
   checkmate::assert_choice(type, c("param", "field"))
   paste("case_definition (`character`)\\cr",
         "A character string that controls which feature store to get data from.",
+        ifelse(type == "field", " Read only.", ""))
+}
+
+
+rd_observable <- function(type = "param") {
+  checkmate::assert_choice(type, c("param", "field"))
+  paste("observable (`character`)\\cr",
+        "The observable to provide prediction for.",
+        "Must match observable in `DiseasyObservables` [R6][R6::R6Class] class.",
+        ifelse(type == "field", " Read only.", ""))
+}
+
+
+rd_prediction_length <- function(type = "param") {
+  checkmate::assert_choice(type, c("param", "field"))
+  paste("prediction_length (`numeric`)\\cr",
+        "The number of days to predict.",
+        "The prediction start is defined by `last_queryable_date` of the `DiseasyObservables` [R6][R6::R6Class] class.",
+        ifelse(type == "field", " Read only.", ""))
+}
+
+
+rd_quantiles <- function(type = "param") {
+  checkmate::assert_choice(type, c("param", "field"))
+  paste("quantiles (`list`(`numeric`))\\cr",
+        "Default NULL.",
+        "If given, results are returned at the quantiles given",
+        ifelse(type == "field", " Read only.", ""))
+}
+
+
+rd_scale <- function(type = "param") {
+  checkmate::assert_choice(type, c("param", "field"))
+  paste("scale (`numeric`)\\cr",
+        "Sets the scale of the season model.",
+        "The scale is the percent wise difference between most active and least active period",
         ifelse(type == "field", " Read only.", ""))
 }
 
@@ -32,18 +77,18 @@ rd_target_schema <- function(type = "param") {
 }
 
 
-rd_start_date <- function(type = "param") {
+rd_training_length <- function(type = "param") {
   checkmate::assert_choice(type, c("param", "field"))
-  paste("start_date (`Date`)\\cr",
-        "Study period start.",
+  paste("training_length (`numeric`)\\cr",
+        "The number of days that should be included in the training of the model.",
         ifelse(type == "field", " Read only.", ""))
 }
 
 
-rd_slice_ts <- function(type = "param") {
+rd_start_date <- function(type = "param") {
   checkmate::assert_choice(type, c("param", "field"))
-  paste("slice_ts (`Date` or `character`)\\cr",
-        "Date to slice the database on (used if source_conn is a database).",
+  paste("start_date (`Date`)\\cr",
+        "Study period start.",
         ifelse(type == "field", " Read only.", ""))
 }
 
@@ -56,9 +101,34 @@ rd_end_date <- function(type = "param") {
 }
 
 
+rd_slice_ts <- function(type = "param") {
+  checkmate::assert_choice(type, c("param", "field"))
+  paste("slice_ts (`Date` or `character`)\\cr",
+        "Date to slice the database on (used if source_conn is a database).",
+        ifelse(type == "field", " Read only.", ""))
+}
+
+
 rd_.data <- function(type = "param") { # nolint: object_name_linter
   checkmate::assert_choice(type, c("param", "field"))
   paste(".data\\cr",
         "The data object to perform the operation on",
         ifelse(type == "field", " Read only.", ""))
 }
+
+
+rd_describe <- "Prints a human readable report of the internal state of the module."
+
+rd_get_results_description() <- paste(
+  "The primary method used to request model results of a given observable at a given aggregation"
+)
+
+rd_get_results_return <- paste(
+  "A `tibble` [tibble::tibble] with predictions at the level specified by aggregation level.",
+  "In addition to aggregation columns, the output has the columns:\\cr",
+  "  date (`Date`) specifying the date of the prediction\\cr",
+  "  realization_id (`character`) giving a unique id for each realization in the ensemble\\cr",
+  "  model (`character`) the name (classname) of the model used to provide the prediction"
+)
+
+rd_get_results_seealso <- "[diseasy::DiseasyObservables]"
