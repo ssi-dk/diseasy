57267ee7ab6c32d0d53bfc77a9fbe4d42dde8f7d
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-09-07 15:55:42 +0200

    feat: add DiseasyActivity

--- R/diseasy-package.R
+++ R/diseasy-package.R
@@ -10,3 +10,23 @@
 #' @import R6
 #' @importFrom rlang caller_env
 NULL
+
+
+#' mg_activity_units
+#'
+#' @name mg_activity_units
+#' @docType data
+#' @author My name \email{blahblah@@roxygen.org}
+#' @references \url{...}
+#' @keywords data
+NULL
+
+
+#' mg_reference_scenario
+#'
+#' @name mg_reference_scenario
+#' @docType data
+#' @author My name \email{blahblah@@roxygen.org}
+#' @references \url{...}
+#' @keywords data
+NULL
