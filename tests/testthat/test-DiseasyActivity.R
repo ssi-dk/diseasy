e2c89b6ceed55e437d7d52a434560206d951dd2a
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-10-04 14:25:06 +0200

    feat: add dk-activity-units data set

--- tests/testthat/test-DiseasyActivity.R
+++ tests/testthat/test-DiseasyActivity.R
@@ -7,23 +7,26 @@ test_that("change_activity works", {
   hash_new_instance <- act$hash
 
   # Load first 10 elements of activity units into the module
-  mg_activity_units_subset <- mg_activity_units[1:10] # mg_activity_units is available from package
-  act$set_activity_units(mg_activity_units_subset)
+  dk_activity_units_subset <- dk_activity_units[1:10] # dk_activity_units is available from package
+  act$set_activity_units(dk_activity_units_subset)
 
   expect_identical(hash_new_instance, act$hash) # hash should not change just because new activity units are loaded
 
   # Try to load ill-defined activity units
-  mg_activity_units_error <- mg_activity_units_subset
-  mg_activity_units_error$basis$work <- NULL
-  expect_error(act$set_activity_units(mg_activity_units_error),
+  dk_activity_units_error <- dk_activity_units_subset
+  dk_activity_units_error$baseline$work <- NULL
+  expect_error(act$set_activity_units(dk_activity_units_error),
                class = "simpleError",
-               regexp = "basis.*work")
+               regexp = "baseline.*work")
 
 
   # Set a scenario
-  scenario_1 <- data.frame(date = as.Date(c("2020-01-01", "2020-03-12", "2020-03-12", "2020-04-15")),
-                           opening      = c("basis",  NA,      "ned2020", "UngUdd.f1.2020"),
-                           closing      = c(NA,       "basis", NA,        NA))
+  scenario_1 <- data.frame(date = as.Date(character(0)), opening = character(0), closing = character(0)) |>
+    dplyr::add_row(date = as.Date("2020-01-01"), opening = "baseline",                         closing = NA) |>
+    dplyr::add_row(date = as.Date("2020-03-12"), opening = NA,                                 closing = "baseline") |>
+    dplyr::add_row(date = as.Date("2020-03-12"), opening = "lockdown_2020",                    closing = NA) |>
+    dplyr::add_row(date = as.Date("2020-04-15"), opening = "secondary_education_phase_1_2020", closing = NA)
+
   act$change_activity(scenario_1)
 
   expect_false(hash_new_instance == act$hash) # hash should change now that a scenario is defined
@@ -32,26 +35,30 @@ test_that("change_activity works", {
   # Check scenario matrix is correctly configured
   ref_scenario <- matrix(0,
                          ncol = 3,
-                         nrow = length(mg_activity_units_subset),
-                         dimnames = list(names(mg_activity_units_subset), as.character(unique(scenario_1$date))))
-  ref_scenario["basis", "2020-01-01"] <- 1
-  ref_scenario["basis", "2020-03-12"] <- 0
-  ref_scenario["ned2020", "2020-03-12"] <- 1
-  ref_scenario["ned2020", "2020-04-15"] <- 1
-  ref_scenario["UngUdd.f1.2020", "2020-04-15"] <- 1
+                         nrow = length(dk_activity_units_subset),
+                         dimnames = list(names(dk_activity_units_subset), as.character(unique(scenario_1$date))))
+  ref_scenario["baseline", "2020-01-01"] <- 1
+  ref_scenario["baseline", "2020-03-12"] <- 0
+  ref_scenario["lockdown_2020", "2020-03-12"] <- 1
+  ref_scenario["lockdown_2020", "2020-04-15"] <- 1
+  ref_scenario["secondary_education_phase_1_2020", "2020-04-15"] <- 1
   ref_scenario <- ref_scenario[rowSums(ref_scenario) > 0, ]
-  attr(ref_scenario, "secret_hash") <- purrr::map_chr(
-    mg_activity_units_subset[c("basis", "ned2020", "UngUdd.f1.2020")],
-    digest::digest) |> digest::digest()
+  attr(ref_scenario, "secret_hash") <- dk_activity_units_subset[c("baseline",
+                                                                  "lockdown_2020",
+                                                                  "secondary_education_phase_1_2020")] |>
+    purrr::map_chr(digest::digest) |>
+    digest::digest()
 
   scenario_matrix <- act$scenario_matrix
   expect_identical(ref_scenario, scenario_matrix)
 
   # Try another way of defining the same scenario:
   act$reset_scenario() # resets the scenario matrix
-  scenario_2 <- data.frame(date = as.Date(c("2020-01-01", "2020-03-12", "2020-04-15")),
-                           opening      = c("basis", "ned2020", "UngUdd.f1.2020"),
-                           closing      = c(NA,      "basis",   NA))
+  scenario_2 <- data.frame(date = as.Date(character(0)), opening = character(0), closing = character(0)) |>
+    dplyr::add_row(date = as.Date("2020-01-01"), opening = "baseline",                            closing = NA) |>
+    dplyr::add_row(date = as.Date("2020-03-12"), opening = "lockdown_2020",                   closing = "baseline") |>
+    dplyr::add_row(date = as.Date("2020-04-15"), opening = "secondary_education_phase_1_2020", closing = NA)
+
   act$change_activity(scenario_2)
   expect_identical(ref_scenario, act$scenario_matrix)
   expect_identical(hash_scenario_1_loaded, act$hash) # hash should be the same as before
@@ -59,9 +66,9 @@ test_that("change_activity works", {
 
   # And without a data.frame as input
   act$reset_scenario() # resets the scenario matrix
-  act$change_activity(date = as.Date(c("2020-01-01", "2020-03-12", "2020-04-15")),
-                      opening      = c("basis", "ned2020", "UngUdd.f1.2020"),
-                      closing      = c(NA,      "basis",   NA))
+  act$change_activity(date = as.Date(c("2020-01-01", "2020-03-12",    "2020-04-15")),
+                      opening      = c("baseline",   "lockdown_2020", "secondary_education_phase_1_2020"),
+                      closing      = c(NA,           "baseline",       NA))
   expect_identical(ref_scenario, act$scenario_matrix)
   expect_identical(hash_scenario_1_loaded, act$hash) # hash should be the same as before
 
@@ -70,29 +77,27 @@ test_that("change_activity works", {
 
   # If we load only the first 9 activity units and the same scenario, we should
   # still get the same scenario matrix and same hash
-  act$set_activity_units(mg_activity_units_subset[1:9])
+  act$set_activity_units(dk_activity_units_subset[1:9])
   act$change_activity(scenario_1)
   expect_identical(ref_scenario, act$scenario_matrix)
   expect_identical(hash_scenario_1_loaded, act$hash) # hash should be the same as before
 
 
   # Test that changing activity fails when a missing activity unit is requested
-  tmp_dates <- c("2020-01-01", "2020-03-12", "2020-03-12", "2020-04-15")
-  tmp_scenario <- data.frame(date = as.Date(tmp_dates),
-                             opening      = c("basis",  NA,      "non_existing_activity_unit", "UngUdd.f1.2020"),
-                             closing      = c(NA,       "basis", NA,        NA))
+  tmp_scenario <- data.frame(date = as.Date(character(0)), opening = character(0), closing = character(0)) |>
+    dplyr::add_row(date = as.Date("2020-01-01"), opening = "baseline",                         closing = NA) |>
+    dplyr::add_row(date = as.Date("2020-03-12"), opening = NA,                                 closing = "baseline") |>
+    dplyr::add_row(date = as.Date("2020-03-12"), opening = "non_existing_activity_unit",       closing = NA) |>
+    dplyr::add_row(date = as.Date("2020-04-15"), opening = "secondary_education_phase_1_2020", closing = NA)
+
   expect_error(act$change_activity(tmp_scenario),
                class = "simpleError",
                regexp = "non_existing_activity_unit")
   rm(tmp_scenario)
 
-  tmp_dates <- c("2020-01-01", "2020-03-12", "2020-04-15")
-  tmp_scenario <- data.frame(date = as.Date(tmp_dates),
-                             opening      = c("basis", "ned2020", "UngUdd.f1.2020"),
-                             closing      = c(NA,      "basis",   NA))
 
   # if we change one of the activity units, the hash should not give the same
-  tmp_activity_units <- mg_activity_units_subset
+  tmp_activity_units <- dk_activity_units_subset
   names(tmp_activity_units) <- c(names(tmp_activity_units[1:3]),
                                  names(tmp_activity_units[10]),
                                  names(tmp_activity_units[5:9]),
@@ -105,21 +110,27 @@ test_that("change_activity works", {
 
 
   # Now we try to load a scenario that will not validate
-  act$set_activity_units(mg_activity_units_subset)
+  act$set_activity_units(dk_activity_units_subset)
   act$change_activity(scenario_1)
-  fail_scenario_1 <- data.frame(date = as.Date(c("2020-05-01",  "2020-05-01",   "2020-06-01",    "2020-06-01")),
-                                opening      = c("LibErv.2020", "Praksis.2020", "Domstole.2020", "PrivArb.f1.2020"),
-                                closing      = c(NA,            NA,             NA,              "Detail.2020"))
+  fail_scenario_1 <- data.frame(date = as.Date(character(0)), opening = character(0), closing = character(0)) |>
+    dplyr::add_row(date = as.Date("2020-05-01"), opening = "liberal_professions_2020",       closing = NA) |>
+    dplyr::add_row(date = as.Date("2020-05-01"), opening = "practices_2020",                 closing = NA) |>
+    dplyr::add_row(date = as.Date("2020-06-01"), opening = "courts_2020",                    closing = NA) |>
+    dplyr::add_row(date = as.Date("2020-06-01"), opening = "private_companies_phase_1_2020", closing = "retail_2020")
+
   expect_error(act$change_activity(fail_scenario_1),  # Fails as expected and reverts state
                class = "simpleError",
-               regexp = "Detail.2020")
+               regexp = "retail_2020")
   expect_identical(ref_scenario, act$scenario_matrix) # Check the state is unchanged
 
 
   # Test that changing activity fails when a missing activity unit is requested
-  fail_scenario_2 <- data.frame(date = as.Date(c("2020-01-01", "2020-03-12", "2020-03-12", "2020-04-15")),
-                                opening      = c("basis",  NA,      "non_existing_activity_unit", "UngUdd.f1.2020"),
-                                closing      = c(NA,       "basis", NA,        NA))
+  fail_scenario_2 <- data.frame(date = as.Date(character(0)), opening = character(0), closing = character(0)) |>
+    dplyr::add_row(date = as.Date("2020-01-01"), opening = "baseline",                         closing = NA) |>
+    dplyr::add_row(date = as.Date("2020-03-12"), opening = NA,                                 closing = "baseline") |>
+    dplyr::add_row(date = as.Date("2020-03-12"), opening = "non_existing_activity_unit",       closing = NA) |>
+    dplyr::add_row(date = as.Date("2020-04-15"), opening = "secondary_education_phase_1_2020", closing = NA)
+
   expect_error(act$change_activity(fail_scenario_2),
                class = "simpleError",
                regexp = "non_existing_activity_unit")
@@ -142,15 +153,19 @@ test_that("change_activity works", {
   expect_identical(colnames(act$risk_matrix), colnames(act$scenario_matrix))
 
   rm(act)
+})
+
+
+test_that("get_scenario_openness works", {
 
   # Test openness
   act <- DiseasyActivity$new(base_scenario = "closed")
-  mg_activity_units_subset <- mg_activity_units[1:10] # mg_activity_units is available from package
-  act$set_activity_units(mg_activity_units_subset)
+  dk_activity_units_subset <- dk_activity_units[1:10] # dk_activity_units is available from package
+  act$set_activity_units(dk_activity_units_subset)
 
-  act$change_activity(date = as.Date(c("2020-01-01", "2020-03-12", "2020-04-15")),
-                      opening      = c("basis", "ned2020", "UngUdd.f1.2020"),
-                      closing      = c(NA,      "basis",   NA))
+  act$change_activity(date = as.Date(c("2020-01-01", "2020-03-12",    "2020-04-15")),
+                      opening      = c("baseline",   "lockdown_2020", "secondary_education_phase_1_2020"),
+                      closing      = c(NA,           "baseline",      NA))
 
   expect_identical(length(act$get_scenario_openness()), 3L) # 3 dates in scenario
   expect_identical(length(act$get_scenario_openness()[[1]]), 4L) # 4 elements
@@ -158,15 +173,20 @@ test_that("change_activity works", {
   #TODO: Test if risks are applied
 
   rm(act)
+})
 
-  # Test BBC contacts
-  act <- DiseasyActivity$new(base_scenario = "closed", contact_basis = bbc_contagion)
-  # mg_activity_units is available from package
-  act$set_activity_units(mg_activity_units[1:10])
 
-  act$change_activity(date = as.Date(c("2020-01-01", "2020-03-12", "2020-04-15")),
-                      opening      = c("basis", "ned2020", "UngUdd.f1.2020"),
-                      closing      = c(NA,      "basis",   NA))
+test_that("contactdata: contact_matrices works", {
+
+  # Test contactdata contacts
+  act <- DiseasyActivity$new(base_scenario = "closed", contact_basis = contact_matrices %.% DK)
+
+  # dk_activity_units is available from package
+  act$set_activity_units(dk_activity_units[1:10])
+
+  act$change_activity(date = as.Date(c("2020-01-01", "2020-03-12",    "2020-04-15")),
+                      opening      = c("baseline",   "lockdown_2020", "secondary_education_phase_1_2020"),
+                      closing      = c(NA,           "baseline",         NA))
 
   # Repeating a previous to see that methods are available
   expect_identical(length(act$get_scenario_openness()), 3L) # 3 dates in scenario
@@ -179,9 +199,13 @@ test_that("change_activity works", {
   expect_identical(dim(act$get_scenario_contacts(age_cuts_lower = c(0))[[1]][[1]]), c(1L, 1L))
 
   rm(act)
+})
+
+
+test_that("dk_reference scenario works", {
 
   ## Test dk_reference scenario
-  act <- DiseasyActivity$new(base_scenario = "dk_reference", contact_basis = bbc_contagion)
+  act <- DiseasyActivity$new(base_scenario = "dk_reference", contact_basis = contact_matrices %.% DK)
   expect_identical(class(act$get_scenario_contacts(age_cuts_lower = c(0, 60))), "list")
   # More tests could be made ... but tested above. The length may change over time so mayby some particular dates.
 
@@ -201,38 +225,38 @@ test_that("set_contact_basis works", {
   hash_new_instance <- act$hash # Store new hash
 
   # Setting basis should change the hash
-  act$set_contact_basis(bbc_contagion)
+  act$set_contact_basis(contact_matrices %.% DK)
 
-  hash_bbc_contagion_loaded <- act$hash # Store new hash
-  expect_false(hash_new_instance == hash_bbc_contagion_loaded) # With contact_basis loaded, hash should change
+  hash_contact_matrices_loaded <- act$hash # Store new hash
+  expect_false(hash_new_instance == hash_contact_matrices_loaded) # With contact_basis loaded, hash should change
 
 
   # Changing basis should give new hash
-  custom_basis <- bbc_contagion
-  custom_basis$description <- 'foo'
+  custom_basis <- contact_matrices %.% DK
+  custom_basis$description <- "foo"
   act$set_contact_basis(custom_basis)
 
-  expect_false(act$hash == hash_bbc_contagion_loaded)
+  expect_false(act$hash == hash_contact_matrices_loaded)
 
   # Check malformed inputs
-  custom_basis <- bbc_contagion
+  custom_basis <- contact_matrices %.% DK
   custom_basis$counts <- custom_basis$counts[-1]
   expect_error(act$set_contact_basis(custom_basis), class = "simpleError",
-               regexp = r"{missing.*elements \{'home'\}}")
+               regexp = r"{missing.*elements.*\{'home'\}}")
 
-  custom_basis <- bbc_contagion
+  custom_basis <- contact_matrices %.% DK
   custom_basis$prop <- custom_basis$prop[-1]
   expect_error(act$set_contact_basis(custom_basis), class = "simpleError",
                regexp = "Must have length 1")
 
-  custom_basis <- bbc_contagion
-  custom_basis$pop <- dplyr::select(custom_basis$pop, 'prop')
+  custom_basis <- contact_matrices %.% DK
+  custom_basis$pop <- dplyr::select(custom_basis$pop, "prop")
   expect_error(act$set_contact_basis(custom_basis), class = "simpleError",
-               regexp = r"{extra.*elements \{'pop'\}}")
+               regexp = r"{extra.*elements.*\{'pop'\}}")
 
-  custom_basis <- bbc_contagion
+  custom_basis <- contact_matrices %.% DK
   expect_error(act$set_contact_basis(custom_basis[-4]), class = "simpleError",
-               regexp = r"{missing.*elements \{'description'\}}")
+               regexp = r"{missing.*elements.*\{'description'\}}")
 
   rm(act)
 })
