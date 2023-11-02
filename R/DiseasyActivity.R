15837e6fc5bdcf2fa4ed54368c9c820c429f44b4
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-11-02 10:56:23 +0100

    fix(DiseasyActivity): add default NULL to $map_population

--- R/DiseasyActivity.R
+++ R/DiseasyActivity.R
@@ -629,7 +629,7 @@ DiseasyActivity <- R6::R6Class( # nolint: object_name_linter
     #   The function computes the proportion of population in the new and old age groups
     # @param age_cuts_lower `r rd_age_cuts_lower`
     # @param population_1yr `r rd_population_1yr`
-    map_population = function(age_cuts_lower, population_1yr) {
+    map_population = function(age_cuts_lower, population_1yr = NULL) {
 
       # Input checks
       coll <- checkmate::makeAssertCollection()
