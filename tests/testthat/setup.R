1bbf3d4561934792270870241779494efcff9b20
Rasmus Skytte Randløv <rske@ssi.dk>
2023-11-16 10:09:42 +0100

    test the new code-coverage wf

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -10,13 +10,89 @@ installed.packages()[, 1] |>
 case_defs <- diseasystore::available_diseasystores() |> stringr::str_remove_all("Diseasystore")
 
 # Create a connection to test on
-tmp_dir <- stringr::str_replace_all(tempdir(), r"{\\}", .Platform$file.sep)
-sqlite_path <- file.path(tmp_dir, "diseasy.sqlite")
-if (file.exists(sqlite_path)) {
-  closeAllConnections()
-  stopifnot("Could not delete SQLite DB before tests" = file.remove(sqlite_path))
+# This function can be copied to your tests/testthat folder
+
+#' Get a list of data base connections to test on
+#' @return
+#'   If you run your tests locally, it returns a list of connections corresponding to conn_list and conn_args
+#'   If you run your tests on GitHub, it return a list of connection corresponding to the environment variables.
+#'   i.e. the GitHub workflows will configure the testing backeds
+#' @importFrom rlang `:=`
+get_test_conns <- function() {
+
+    # Check if we run remotely
+    running_locally <- !identical(Sys.getenv("CI"), "true")
+
+    # Define list of connections to check
+    if (running_locally) {
+
+        # Define our local connection backends
+        conn_list <- list(
+            # Backend string = package::function
+            "SQLite"     = "RSQLite::SQLite",
+            "PostgreSQL" = "RPostgres::Postgres"
+        )
+
+    } else {
+        # Use the connection configured by the remote
+        conn_list <- tibble::lst(
+            !!Sys.getenv("BACKEND", unset = "SQLite") := !!Sys.getenv("BACKEND_DRV", unset = "RSQLite::SQLite")         # nolint: object_name_linter
+        )
+    }
+
+    # Define list of args to conns
+    if (running_locally) {
+
+        # Define our local connection arguments
+        conn_args <- list(
+            # Backend string = list(named args)
+            "SQLite" = list(dbname = tempfile())
+        )
+
+    } else {
+        # Use the connection configured by the remote
+        conn_args <- tibble::lst(
+            !!Sys.getenv("BACKEND", unset = "SQLite") :=                                                                # nolint: object_name_linter
+              !!Sys.getenv("BACKEND_ARGS", unset = "list(dbname = tempfile())")                                         # nolint: object_name_linter
+        ) |>
+        purrr::map(~ eval(parse(text = .)))
+    }
+
+
+    get_driver <- function(x = character(), ...) {
+        if (!grepl(".*::.*", x)) stop("Package must be specified with namespace (e.g. RSQLite::SQLite)!\n",
+                                        "Received: ", x)
+        parts <- strsplit(x, "::")[[1]]
+
+        # Skip unavailable packages
+        if (!requireNamespace(parts[1], quietly = TRUE)) {
+            return()
+        }
+
+        drv <- getExportedValue(parts[1], parts[2])
+
+        tryCatch(suppressWarnings(SCDB::get_connection(drv = drv(), ...)),  # We expect a warning if no tables are found
+                error = function(e) {
+                    NULL # Return NULL, if we cannot connect
+                })
+    }
+
+    # Create connection generator
+    conn_configuration <- dplyr::left_join(
+        tibble::tibble(backend = names(conn_list), conn_list = unname(unlist(conn_list))),
+        tibble::tibble(backend = names(conn_args), conn_args),
+        by = "backend"
+    )
+
+    test_conns <- purrr::pmap(conn_configuration, ~ purrr::partial(get_driver, x = !!..2, !!!..3)())
+    names(test_conns) <- conn_configuration$backend
+    test_conns <- purrr::discard(test_conns, is.null)
+
+    return(test_conns)
 }
-test_conn <- \() DBI::dbConnect(RSQLite::SQLite(), sqlite_path)
+
+
+test_conn <- \() get_test_conns()[1]
 options(diseasy.conn = test_conn)
 
 # Then we download the first n rows of the google data set of interest
