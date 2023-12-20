3da295926614d9f5484c67641717504cb8896152
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2023-12-20 10:56:47 +0100

    chore: clean up NAMESPACE

--- R/diseasy-package.R
+++ R/diseasy-package.R
@@ -1,12 +1,2 @@
 #' @keywords internal
 "_PACKAGE"
-
-#' @importFrom digest digest
-#' @importFrom dplyr as_label
-#' @import diseasystore
-#' @import lgr
-#' @importFrom lubridate today
-#' @importFrom pracma logseq
-#' @import R6
-#' @importFrom rlang caller_env
-NULL
