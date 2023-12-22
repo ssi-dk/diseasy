5608ed9ecf24f2c11afa7b59b1e4e4f4146a1574
RasmusSkytte <rasmusskytteeriksen@gmail.com>
2023-12-22 09:23:44 +0000

    chore: synced file(s) with ssi-dk/diseasystore

--- R/0_linters.R
+++ R/0_linters.R
@@ -1,14 +1,17 @@
-#' The custom linters of `diseasy`
-#'
+#' @title
+#'   The custom linters of `diseasy`
+#' @description
+#'   A curated list of linters to ensure adherence to the `diseasy` documentation and code standards
 #' @name diseasy_linters
 #' @examples
 #'   diseasy_code_linters()
 #' @return A list of linters
-#' @export
+#' @noRd
 diseasy_code_linters <- function() {
   linters <- list(
-    non_ascii_linter(),
-    todo_linter()
+    nolint_position_linter(120),
+    nolint_line_length_linter(120),
+    non_ascii_linter()
   )
 
   return(linters)
@@ -16,7 +19,146 @@ diseasy_code_linters <- function() {
 
 
 #' @rdname diseasy_linters
+#' @description
+#' nolint_position_linter: Ensure `nolint:` statements occur after the character limit
+#'
+#' @param length maximum line length allowed. Default is 80L (Hollerith limit).
+#' @returns A list of `lintr::Lint`
+#' @examples
+#' ## nolint_position_linter
+#' # will produce lints
+#' lintr::lint(
+#'   text = paste0(strrep("x", 15L), "# nolint: object_name_linter"),
+#'   linters = c(nolint_position_linter(length = 20L), lintr::object_name_linter())
+#' )
+#'
+#' # okay
+#' lintr::lint(
+#'   text = paste0(strrep("x", 20L), "# nolint: object_name_linter"),
+#'   linters = c(nolint_position_linter(length = 20L), lintr::object_name_linter())
+#' )
+#'
+#' @seealso
+#' - [lintr::linters] for a complete list of linters available in lintr.
+#' - <https://style.tidyverse.org/syntax.html#long-lines>
 #' @importFrom rlang .data
+#' @noRd
+nolint_position_linter <- function(length = 80L) {
+  general_msg <- paste("`nolint:` statements start at", length + 1, "characters.")
+
+  lintr::Linter(
+    function(source_expression) {
+
+      # Only go over complete file
+      if (!lintr::is_lint_level(source_expression, "file")) {
+        return(list())
+      }
+
+      nolint_info <- source_expression$content |>
+        stringr::str_locate_all(stringr::regex(r"{# *nolint}", ignore_case = TRUE))
+
+      nolint_info <- purrr::map2(
+        nolint_info,
+        seq_along(nolint_info),
+        ~ dplyr::mutate(as.data.frame(.x), line_number = .y)
+      ) |>
+        purrr::reduce(rbind) |>
+        dplyr::filter(!is.na(.data$start)) |>
+        dplyr::filter(.data$start <= length)
+
+      purrr::pmap(
+        nolint_info,
+        \(start, end, line_number) {
+          lintr::Lint(
+            filename = source_expression$filename,
+            line_number = line_number,
+            column_number = start,
+            type = "style",
+            message = paste(general_msg, "This statement starts at", start, "characters"),
+            line = source_expression$file_lines[line_number],
+            ranges = list(c(start, end))
+          )
+        }
+      )
+    }
+  )
+}
+
+
+#' @rdname diseasy_linters
+#' @description
+#' nolint_line_length_linter: Ensure lines adhere to a given character limit, ignoring `nolint` statements
+#'
+#' @param length maximum line length allowed. Default is 80L (Hollerith limit).
+#' @examples
+#' ## nolint_line_length_linter
+#' # will produce lints
+#' lintr::lint(
+#'   text = paste0(strrep("x", 25L), "# nolint: object_name_linter."),
+#'   linters = c(nolint_line_length_linter(length = 20L), lintr::object_name_linter())
+#' )
+#'
+#' # okay
+#' lintr::lint(
+#'   text = paste0(strrep("x", 20L), "# nolint: object_name_linter."),
+#'   linters = c(nolint_line_length_linter(length = 20L), lintr::object_name_linter())
+#' )
+#'
+#' @importFrom rlang .data
+#' @noRd
+nolint_line_length_linter <- function(length = 80L) {
+  general_msg <- paste("Lines should not be more than", length, "characters.")
+
+  lintr::Linter(
+    function(source_expression) {
+
+      # Only go over complete file
+      if (!lintr::is_lint_level(source_expression, "file")) {
+        return(list())
+      }
+
+      nolint_regex <- r"{# ?no(lint|cov) ?(start|end)?:?.*}"
+
+      file_lines_nolint_excluded <- source_expression$file_lines |>
+        purrr::map_chr(\(s) stringr::str_remove(s, nolint_regex))
+
+      line_lengths <- nchar(file_lines_nolint_excluded)
+      long_lines <- which(line_lengths > length)
+      Map(function(long_line, line_length) {
+        lintr::Lint(
+          filename = source_expression$filename,
+          line_number = long_line,
+          column_number = length + 1L, type = "style",
+          message = paste(general_msg, "This line is", line_length, "characters."),
+          line = source_expression$file_lines[long_line],
+          ranges = list(c(1L, line_length))
+        )
+      }, long_lines, line_lengths[long_lines])
+    }
+  )
+}
+
+
+#' @rdname diseasy_linters
+#' @description
+#' non_ascii_linter: Ensure the code base only contains ASCII symbols
+#'
+#' @examples
+#' ## non_ascii_linter
+#' # will produce lints
+#' lintr::lint(
+#'   text = "a-å",                                                                                                      # nolint: non_ascii_linter
+#'   linters = non_ascii_linter()
+#' )
+#'
+#' # okay
+#' lintr::lint(
+#'   text = "a-z",                                                                                                      # nolint: non_ascii_linter
+#'   linters = non_ascii_linter()
+#' )
+#'
+#' @importFrom rlang .data
+#' @noRd
 non_ascii_linter <- function() {
   general_msg <- paste("Code should not contain non-ASCII characters")
 
@@ -50,48 +192,6 @@ non_ascii_linter <- function() {
             column_number = start,
             type = "style",
             message = paste(general_msg, "non-ASCII character found"),
-            line = source_expression$file_lines[line_number],
-            ranges = list(c(start, end))
-          )
-        }
-      )
-    }
-  )
-}
-
-
-#' @rdname diseasy_linters
-todo_linter <- function() {
-  general_msg <- paste("`TODO` statements should not be kept in code base:")
-
-  lintr::Linter(
-    function(source_expression) {
-
-      # Only go over complete file
-      if (!lintr::is_lint_level(source_expression, "file")) {
-        return(list())
-      }
-
-      todo_info <- source_expression$file_lines |>
-        stringr::str_locate_all(stringr::regex(r"{(?<=\s|^)todos?:?(?=\s|$)}", ignore_case = TRUE))
-
-      todo_info <- purrr::map2(
-        todo_info,
-        seq_along(todo_info),
-        ~ dplyr::mutate(as.data.frame(.x), line_number = .y)
-      ) |>
-        purrr::reduce(rbind) |>
-        dplyr::filter(!is.na(.data$start))
-
-      purrr::pmap(
-        todo_info,
-        \(start, end, line_number) {
-          lintr::Lint(
-            filename = source_expression$filename,
-            line_number = line_number,
-            column_number = start,
-            type = "style",
-            message = paste(general_msg, "`TODO` statement found"),
             line = source_expression$file_lines[line_number],
             ranges = list(c(start, end))
           )
--- tests/testthat/test-0_R6_utils.R
+++ tests/testthat/test-0_R6_utils.R
@@ -1,29 +1,60 @@
+test_that("printr works", {
+
+  # Check that printr works without file printing
+  # 1)
+  checkmate::expect_character(capture.output(printr("test string")),               pattern = r"{test string}")
+  # 2)
+  checkmate::expect_character(capture.output(printr("test1", "test2")),            pattern = r"{test1test2}")
+  # 3)
+  checkmate::expect_character(capture.output(printr("test1", "test2", sep = " ")), pattern = r"{test1 test2}")
+
+  # Check that printr works with file printing
+  test_file <- tempfile()
+
+  # 1)
+  checkmate::expect_character(capture.output(printr("test string",    file = test_file)),
+                              pattern = r"{test string}")
+  checkmate::expect_character(readLines(test_file),
+                              pattern = r"{test string}")
+  file.remove(test_file)
+
+
+  # 2)
+  checkmate::expect_character(capture.output(printr("test1", "test2", file = test_file)),
+                              pattern = r"{test1test2}")
+  checkmate::expect_character(readLines(test_file),
+                              pattern = r"{test1test2}")
+  file.remove(test_file)
+
+
+  # 3)
+  checkmate::expect_character(capture.output(printr("test1", "test2", file = test_file, sep = " ")),
+                              pattern = r"{test1 test2}")
+  checkmate::expect_character(readLines(test_file),
+                              pattern = r"{test1 test2}")
+  file.remove(test_file)
+})
+
+
 test_that("diseasyoption works", {
 
-  # Store the current options
-  opts <- options(
-    "diseasystore.target_schema" = "",
-    "diseasystore.DiseasystoreGoogleCovid19.target_schema" = ""
-  )
-
   # Check that diseasyoption works for default values
-  expect_equal(diseasyoption("target_schema"), NULL)
+  expect_null(diseasyoption("non_existent_option"))
 
-  options("diseasystore.target_schema" = "target_schema_1")
-  expect_equal(diseasyoption("target_schema"), "target_schema_1")
+  withr::local_options("diseasystore.target_schema" = target_schema_1)
+  expect_identical(diseasyoption("target_schema"), target_schema_1)
 
   # Check that it works for child classes
   ds <- DiseasystoreGoogleCovid19$new(target_conn = DBI::dbConnect(RSQLite::SQLite()))
-  expect_equal(diseasyoption("target_schema", "DiseasystoreGoogleCovid19"), "target_schema_1")
-  expect_equal(diseasyoption("target_schema", ds), "target_schema_1")
+  expect_identical(diseasyoption("target_schema", "DiseasystoreGoogleCovid19"), target_schema_1)
+  expect_identical(diseasyoption("target_schema", ds), target_schema_1)
 
-  options("diseasystore.DiseasystoreGoogleCovid19.target_schema" = "target_schema_2")
-  expect_equal(diseasyoption("target_schema", "DiseasystoreGoogleCovid19"), "target_schema_2")
-  expect_equal(diseasyoption("target_schema", ds), "target_schema_2")
+  withr::local_options("diseasystore.DiseasystoreGoogleCovid19.target_schema" = target_schema_2)
+  expect_identical(diseasyoption("target_schema", "DiseasystoreGoogleCovid19"), target_schema_2)
+  expect_identical(diseasyoption("target_schema", ds), target_schema_2)
 
-  # Reset options
-  options(opts)
-  rm(opts)
+  rm(ds)
+  invisible(gc())
 })
 
 
@@ -67,7 +98,7 @@ test_that("parse_diseasyconn works", {
   null_conn <- NULL
 
   # Test inputs for source_conn
-  expect_no_condition(conn <- parse_diseasyconn(valid_function_conn, type = "source_conn"))
+  conn <- expect_no_condition(parse_diseasyconn(valid_function_conn, type = "source_conn"))
   checkmate::expect_class(conn, "DBIConnection")
   DBI::dbDisconnect(conn)
 
@@ -76,19 +107,21 @@ test_that("parse_diseasyconn works", {
                class = "simpleError", regexp = "`conn` could not be parsed!")
 
 
-  expect_no_condition(conn <- parse_diseasyconn(valid_dbi_conn, type = "source_conn"))
+  conn <- expect_no_condition(parse_diseasyconn(valid_dbi_conn, type = "source_conn"))
   expect_identical(conn, valid_dbi_conn)
+  checkmate::expect_class(conn, "DBIConnection")
 
-  expect_no_condition(conn <- parse_diseasyconn(valid_str_conn, type = "source_conn"))
+
+  conn <- expect_no_condition(parse_diseasyconn(valid_str_conn, type = "source_conn"))
   expect_identical(conn, valid_str_conn)
 
 
-  expect_no_condition(conn <- parse_diseasyconn(null_conn, type = "source_conn"))
+  conn <- expect_no_condition(parse_diseasyconn(null_conn, type = "source_conn"))
   expect_null(conn)
 
 
   # Test inputs for target_conn
-  expect_no_condition(conn <- parse_diseasyconn(valid_function_conn, type = "target_conn"))
+  conn <- expect_no_condition(parse_diseasyconn(valid_function_conn, type = "target_conn"))
   checkmate::expect_class(conn, "DBIConnection")
   DBI::dbDisconnect(conn)
 
@@ -97,14 +130,18 @@ test_that("parse_diseasyconn works", {
                class = "simpleError", regexp = "`conn` could not be parsed!")
 
 
-  expect_no_condition(conn <- parse_diseasyconn(valid_dbi_conn, type = "target_conn"))
+  conn <- expect_no_condition(parse_diseasyconn(valid_dbi_conn, type = "target_conn"))
   expect_identical(conn, valid_dbi_conn)
+  checkmate::expect_class(conn, "DBIConnection")
 
 
   expect_error(parse_diseasyconn(valid_str_conn, type = "target_conn"),
                class = "simpleError", regexp = "`conn` could not be parsed!")
 
 
-  expect_no_condition(conn <- parse_diseasyconn(null_conn, type = "target_conn"))
+  conn <- expect_no_condition(parse_diseasyconn(null_conn, type = "target_conn"))
   expect_null(conn)
+
+  # Test clean up
+  DBI::dbDisconnect(valid_dbi_conn)
 })
