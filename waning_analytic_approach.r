70b2f0e12a9ec0ed8460a4f37149fe32e0ee6d5b
SofiaOtero <myrup.otero@gmail.com>
2024-04-18 12:33:18 +0200

    Fixed to be used with occupancy function and adds elbow method

--- waning_analytic_approach.r
+++ waning_analytic_approach.r
@@ -1,90 +1,131 @@
-
-# Setup parameters
-t <- seq(from = 0, to = 50, by = 1)
-K = 20
-tau <- 50/K # Average time spend in each compartment
-lambda <- 1/tau # Rate of transition between compartments
+source("R/0_hypoexponential.R")
+source("waning_occupancy_probability.R")
 
 
-# The probability that an "event" has not happened yet (due to lower.tail = FALSE)
-stage_cdf <- \(t,K,lambda) stats::pgamma(t, shape = K, rate = lambda, lower.tail = FALSE)
+# Using same rate
+tau <- 50 / 3 # Average time spend in each compartment
+t <- seq(0, 5 * tau, by = 1) 
+K <- 10                                                                                                                  # nolint: object_name_linter
+lambda <- K / (3 * tau) # Rate of transition between compartments
 
-# Gamma cumulative distribution function for K compartments
-time_df <- function(t, K, lambda) {
-  K_stages <- seq(from = 1, to = K-1, by = 1)
-  
-  cdf_tibble <- purrr::map(K_stages, ~ {
-    stage_cdf(t, .x, lambda) |>
-      as.data.frame() |>
-      stats::setNames(.x)
-  }) |>
-    purrr::list_cbind()
-  return(cdf_tibble)
-}
+# Using different rates
+rate <- rev(seq(K - 1)) # Use decreasing rates
+rate <- rate * sum(1 / rate) / (K / lambda) # Normalize so total waiting time is equal between problems
 
-# The probability that an event happens in each timepoint in each compartment
-stage_prob <- function(t, K, lambda) {
-  cdf_df <- time_df(t,K,lambda)
-  
-  prK <- cdf_df - cbind(rep(0,length(t)),cdf_df[,1:(K-2)])
-  last_c <- 1 - rowSums(prK)
-  return(cbind(prK,last_c))
-}
-
-prk_df <- stage_prob(t, K, lambda)
-
-# Create gamma values - to corresponding amount of compartments
 gamma_f <- \(K) 1-seq(K)/K
-gamma_values <- gamma_f(K)
 
-# Function to be optimized for
-sigmoid_function <- \(t)  exp(-(t - 20)/4) / (1 + exp(-(t - 20)/4))
-
-#Element-wise multiplication - add gamma_values to corresponding K-compartment
-stages_sum <- function(prk_df, gamma_values) {
-  as.matrix(prk_df) %*% gamma_values
+create_params <- function(K, rate) {
+  gamma_values <- gamma_f(K)
+  params <- c(gamma_values, rate)
+  return(params)
 }
+
+params_selected <- create_params(K,rate)
+
+# Functions to be optimized for
+sigmoid_function <- \(t)  exp(-(t - 20)/4) / (1 + exp(-(t - 20)/4))
+exponential_function <- \(t) exp(-t/10)
 
 # The target function we want to approximate our data to
 target_function <- function(f_type, time_points) {
   purrr::map_dbl(time_points, f_type)
 }
 
-# Precompute target function to speed up
 function_type <- sigmoid_function
-target <- target_function(function_type, t)
-plot(target)
 
-# Finds dif from approximation and target function
-objective_function_cum <- function(prk_df, gamma_values) {
-  sum(((as.matrix(prk_df) %*% cumprod(gamma_values)) - target)^2)
+### Object function
+obj_function <- function(K, t, params, f_type) {
+  
+  gamma_values <- params[1:K]
+  rate <- params[(K+1):length(params)]
+  
+  # Call occupancy probability function
+  prk_df <- occupancy_probability(rate, K, t)
+  prk_matrix <<- do.call(cbind, prk_df)
+  
+  # Target function to approximate for
+  target <<- target_function(f_type, t)
+  
+  # Element-wise multiplication - add gamma_values to corresponding K-compartments and row sum
+  approx_matrix <<- prk_matrix %*% cumprod(gamma_values)
+    
+  # Finds diff from approximation and target function
+  result <- sum((approx_matrix - target)^2)
+  
 }
 
-# Minimize difference between approximation and target function
-res2 <- stats::optim(gamma_values, \(gamma) objective_function_cum(prk_df, gamma), lower = 0, upper = 1, method = "L-BFGS-B")
 
-plot(t, stages_sum(prk_df, cumprod(res2$par)))
+##### Minimize difference between approximation and target function
+
+# Fixed lambda/rate, only optimizing on gamma
+K <- 10
+params_selected <- create_params(K, lambda)
+res <- stats::optim(params_selected[1:K], \(params) obj_function(K, t, c(params, lambda), function_type), lower = 0, upper = 1, method = "L-BFGS-B")
+
+plot(t, prk_matrix %*% cumprod(res$par[1:K]))
 lines(t, target)
 
 
-# K vs. values from obj. function (Elbow method)
+# Only one lambda/rate to be optimized on
+K <- 10
+params_selected <- create_params(K, lambda)
+res <- stats::optim(params_selected, \(params) obj_function(K, t, params, function_type), lower = 0, upper = 1, method = "L-BFGS-B")
 
-K_seq = seq(from = 2, to = 30, by = 1)
+plot(t, prk_matrix %*% cumprod(res$par[1:K]))
+lines(t, target)
 
-objective_values <- function(K_seq, objective_func) {
- 
+# K - 1 lambda/rates to be optimized on
+
+params_selected <- create_params(K, rate)
+res <- stats::optim(params_selected, \(params) obj_function(K, t, params, function_type), lower = 0, upper = 1, method = "L-BFGS-B")
+
+plot(t, prk_matrix %*% cumprod(res$par[1:K]))
+lines(t, target)
+
+
+
+#### K vs. values from obj. function (Elbow method)
+
+K_seq = seq(from = 3, to = 10, by = 1)
+
+objective_values <- function(K_seq, f_type, diff_rate) {
+  
+  tau <- 50 / 3
+  t <- seq(0, 5 * tau, by = 1)
+  
   obj_list <- purrr::map(K_seq, ~ {
-    gamma_v <- gamma_f(.x)
-    prk_df <- stage_prob(t, K = .x, lambda = .x / 50)
-    obj <- stats::optim(gamma_v, \(gamma) objective_func(prk_df, gamma), lower = 0, upper = 1, method = "L-BFGS-B")
+  
+    lambda <- .x / (3 * tau)
+    
+    if (diff_rate == "True") {
+      rate <- rev(seq_len(.x - 1))
+      rate <- rate * sum(1 / rate) / (.x / lambda)
+    } else {
+      rate = lambda
+    }
+    
+    params_selected <- create_params(.x, rate)
+    
+    if (diff_rate == "Fixed") {
+      obj <- stats::optim(params_selected[1:.x], \(params) obj_function(.x, t, c(params, rate), function_type), lower = 0, upper = 1, method = "L-BFGS-B")
+    } else {
+      obj <- stats::optim(params_selected, \(params) obj_function(.x, t, params, f_type), lower = 0, upper = 1, method = "L-BFGS-B")
+    }
+    
     tibble::tibble(K = .x, value = obj$value)
   })
+    
   return(dplyr::bind_rows(obj_list))
 }
 
-test <- objective_values(K_seq, objective_function_cum)
+test <- objective_values(K_seq, function_type, diff_rate = "False")
+test_diff <- objective_values(K_seq, function_type, diff_rate = "True")
+test_fixed <- objective_values(K_seq, function_type, diff_rate = "Fixed")
 
-ggplot2::ggplot(test, aes(x = K, y = value)) +
-  ggplot2::geom_point() +
-  ggplot2::scale_x_continuous(breaks = seq(1, max(K_seq), by = 1)) +
+ggplot2::ggplot() +
+  ggplot2::geom_line(data = test, aes(x = K, y = value), color = "blue") +
+  ggplot2::geom_line(data = test_diff, aes(x = K, y = value), color = "red") +
+  ggplot2::geom_line(data = test_fixed, aes(x = K, y = value), color = "green") +
+  ggplot2::scale_x_continuous(breaks = seq(1, max(test$K), by = 1)) +
   ggplot2::labs(x = "K", y = "Obj. function value")
+
