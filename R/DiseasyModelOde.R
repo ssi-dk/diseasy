72ee3c8bffb5e5f27513de121a50e3652b0060a0
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-06-07 08:59:16 +0200

    WIP - DiseasyModelOde

---
+++ R/DiseasyModelOde.R
@@ -0,0 +1,146 @@
+#' @title Meta module for the regression class of models
+#'
+#' @description TODO
+#' @export
+DiseasyModelOde <- R6::R6Class(
+  classname = "DiseasyModelOde",
+  inherit = DiseasyModel,
+
+  public = list(
+
+    #' @description
+    #'   Creates a new instance of the `DiseasyModelOde` [R6][R6::R6Class] class.
+    #'   This module is typically not constructed directly but rather through `DiseasyModelOde*` classes,
+    #'   such as [DiseasyModelOdeSeir].
+    #' @param ...
+    #'   parameters sent to `DiseasyModel` [R6][R6::R6Class] constructor.
+    #' @details
+    initialize = function(training_length, ...) {
+
+      coll <- checkmate::makeAssertCollection()
+      checkmate::assert_number(training_length, add = coll)
+      checkmate::reportAssertions(coll)
+
+      # Store the given parameters
+      private$.parameters$training_length <- training_length
+
+      # Pass arguments to the DiseasyModel initializer
+      super$initialize(...)
+    },
+
+
+    # Roxygen has only limited support for R6 docs currently, so we need to do some tricks for the documentation
+    # of get_results
+    #' @description `r .get_results_description`
+    #' @template observable
+    #' @template prediction_length
+    #' @template quantiles
+    #' @template aggregation
+    #' @return `r .get_results_return`
+    #' @seealso `r .get_results_seealso`
+    get_results = function(observable, prediction_length, quantiles = NULL, aggregation = NULL) {
+      coll <- checkmate::makeAssertCollection()
+      checkmate::assert_true(!is.null(self %.% observables), add = coll)
+      checkmate::assert_choice(observable, self %.% observables %.% available_observables, add = coll)
+      checkmate::assert_number(prediction_length, add = coll)
+      checkmate::reportAssertions(coll)
+
+      # Look in the cache for data
+      hash <- private$get_hash()
+      if (!private$is_cached(hash)) {
+
+        # Get the observable at the aggregation level
+        data <- self$get_training_data(observable, aggregation)
+
+        # Adjust the formula for the observable and aggregation
+        # By default, the formula is return without changes.
+        # Only if the subclass overloads update_formula are the changes done here
+        formula <- stats::as.formula(glue::glue(self$formula)) |>
+          private$update_formula(aggregation)
+
+
+        # Perform the regression fitting
+        regression_fit <- private$fit_regression(data, formula)
+
+        # Simulating glm model
+        # Get all combinations of columns
+        prototype_data <- data |>
+          dplyr::filter(dplyr::if_any(.cols = !!observable, .fns = purrr::negate(is.na))) |>
+          dplyr::select(!tidyselect::all_of(c(observable, "date", "t"))) |>
+          dplyr::distinct_all()
+
+        new_data <- purrr::map(c(-(seq(self$parameters$training_length) - 1), seq(prediction_length)),
+                               ~ dplyr::mutate(prototype_data,
+                                               t = .x,
+                                               date = max(data$date) + lubridate::days(t))) |>
+          purrr::reduce(dplyr::union_all) |>
+          dplyr::relocate(date) # Move "date" column to first column
+
+
+        # Get prediction from model
+        prediction <- private$get_prediction(regression_fit, new_data, quantiles)
+
+        # Finalize output
+        prediction <- prediction |>
+          dplyr::select(!t) |> # Delete the time row
+          dplyr::rename(!!observable := observable) |>
+          dplyr::mutate(model = paste(c(class(self)[1], private$label), sep = " - ")) # Add meta information to the data
+
+        # Store in cache
+        private$cache(hash, prediction)
+      }
+
+      # Write to the log
+      private$report_get_results(observable, aggregation, prediction_length, hash)
+
+      # Return
+      return(private$cache(hash))
+    }
+  ),
+
+
+  active = list(
+    #' @field formula (`formula`)\cr
+    #'   The base formula of the module. Aggregation features extend this base formula. Read-only.
+    formula = function(value) {
+      if (missing(value)) {
+        return(private %.% .formula)
+      } else {
+        private$read_only_error("formula")
+      }
+    },
+
+    #' @field family (`family`)\cr
+    #'   The family used in the regression fit (see `glm` or `brms`). Read-only.
+    family = function(value) {
+      if (missing(value)) {
+        return(private %.% .family)
+      } else {
+        private$read_only_error("family")
+      }
+    }
+  ),
+
+  private = list(
+    .formula = NULL,
+    .family = NULL,
+
+    fit_regression = function(data, formula) {
+      private$not_implemented_error("`$fit_regression` must be implemented in inheiriting class")
+    },
+
+    get_prediction = function(regression_fit, new_data, quantiles) {
+      private$not_implemented_error("`$get_prediction` must be implemented in inheiriting class")
+    },
+
+    update_formula = function(formula, aggregation) {
+      private$not_implemented_error("`$update_formula` must be implemented in inheiriting class")
+    },
+
+    report_regression_fit = function(regressor, formula, family, hash) {
+      private$lg$info("Running {regressor} with formula: {format(formula)}",
+                      " and family: {purrr::pluck(family, 'family', .default = family)}",
+                      " (hash: {hash})")
+    }
+  ),
+)
