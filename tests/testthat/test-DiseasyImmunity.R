5145df28f3c4cffb9fb8b415287f6f3dbeb4b237
SofiaOtero <myrup.otero@gmail.com>
2024-06-19 16:24:45 +0200

    Vignette for the immunity module and some env and other fixes

--- tests/testthat/test-DiseasyImmunity.R
+++ tests/testthat/test-DiseasyImmunity.R
@@ -3,17 +3,17 @@ test_that("initialize works", {
   # Creating an empty module
   im <- DiseasyImmunity$new()
 
-  expect_equal(im$model, im$use_no_waning())
+  expect_equal(im$model, im$set_no_waning())
 
   rm(im)
 })
 
-test_that("Available waning models equals 6", {
+test_that("Available waning models equals 5", {
 
   # Creating an empty module
   im <- DiseasyImmunity$new()
 
-  expect_length(im$available_waning_models, 6)
+  expect_length(im$available_waning_models, 5)
 
   rm(im)
 })
@@ -23,13 +23,29 @@ test_that("use_waning_models works with known model", {
   im1 <- DiseasyImmunity$new()
   im2 <- DiseasyImmunity$new()
 
-  im1$use_exponential_waning()
-  im2$use_waning_model("exponential_waning")
+  im1$set_exponential_waning()
+  im2$set_waning_model("exponential_waning")
 
   expect_equal(im1, im2)
 
-  im2$use_waning_model("sigmoidal_waning")
+  im2$set_waning_model("sigmoidal_waning")
   expect_false(identical(im1, im2))
 
   rm(im1, im2)
+})
+
+test_that("obj_value is correct nomatter input type for approach", {
+  im1 <- DiseasyImmunity$new()
+  im2 <- DiseasyImmunity$new()
+  im3 <- DiseasyImmunity$new()
+
+  im1$set_exponential_waning()
+  im2$set_exponential_waning()
+  im3$set_exponential_waning()
+
+  im1$approximate_compartmental(N = 2)
+  im2$approximate_compartmental(approach = "rate_equal", N = 2)
+  im3$approximate_compartmental(approach = 1, N = 2)
+
+  expect_equal(im1$obj_value, im2$obj_value, im3$obj_value)
 })
