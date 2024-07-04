84fc2f97d28a3cadc969f7fddb337a87c7e5102b
SofiaOtero <myrup.otero@gmail.com>
2024-07-04 15:09:46 +0200

    Now works with N = 1 (compartments)

--- tests/testthat/test-DiseasyImmunity.R
+++ tests/testthat/test-DiseasyImmunity.R
@@ -13,7 +13,7 @@ test_that("Available waning models equals 5", {
   # Creating an empty module
   im <- DiseasyImmunity$new()
 
-  expect_length(im$available_waning_models, 5)
+  expect_length(im$available_waning_models, 6)
 
   rm(im)
 })
@@ -37,15 +37,9 @@ test_that("use_waning_models works with known model", {
 test_that("obj_value is correct nomatter input type for approach", {
   im1 <- DiseasyImmunity$new()
   im2 <- DiseasyImmunity$new()
-  im3 <- DiseasyImmunity$new()
 
   im1$set_exponential_waning()
   im2$set_exponential_waning()
-  im3$set_exponential_waning()
 
-  im1$approximate_compartmental(N = 2)
-  im2$approximate_compartmental(approach = "rate_equal", N = 2)
-  im3$approximate_compartmental(approach = 1, N = 2)
-
-  expect_equal(im1$obj_value, im2$obj_value, im3$obj_value)
+  expect_equal(im1$approximate_compartmental(N = 2), im2$approximate_compartmental(approach = "rate_equal", N = 2))
 })
