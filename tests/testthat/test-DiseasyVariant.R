aea6622840bbe47450b9ac2c8574d98ebf4e7a1a
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-07-11 14:36:33 +0200

    test(DiseasyVariant): Add tests for the initial version

---
+++ tests/testthat/test-DiseasyVariant.R
@@ -0,0 +1,94 @@
+test_that("initialize works", {
+
+  # Creating an empty module
+  variant <- DiseasyVariant$new()
+  expect_null(variant %.% variants)
+
+  rm(variant)
+})
+
+
+test_that("$add_variant() works", {
+
+  # Creating an empty module
+  variant <- DiseasyVariant$new()
+  expect_null(variant %.% variants)
+  hash_new_instance <- variant$hash # Store the current hash
+
+
+  # Add a new variant
+  variant$add_variant(name = "WT")
+  expect_identical(variant %.% variants, list("WT" = list()))
+  hash_wt <- variant$hash
+  expect_false(identical(hash_wt, hash_new_instance))
+
+
+  # Add with all characteristics
+  variant$add_variant(
+    name = "Mutant",
+    characteristics = list(
+      "relative_infection_risk" = 1.2,
+      "cross_immunity" = c("WT" = 0.5),
+      "introduction_date" = Sys.Date()
+    )
+  )
+  expect_false(identical(variant$hahs, hash_new_instance))
+  expect_false(identical(variant$hahs, hash_wt))
+
+
+  # Check malformed input
+  expect_error(
+    variant$add_variant(name = "WT"),
+    "Must be disjunct from"
+  )
+  expect_error(
+    variant$add_variant(name = 1),
+    "Must be of type 'character'"
+  )
+  expect_error(
+    variant$add_variant(name = "variant", characteristics = list("relative_infection_risk" = "a")),
+    "Must be of type 'number'"
+  )
+  expect_error(
+    variant$add_variant(name = "variant", characteristics = list("cross_immunity" = c("a" = "b"))),
+    "Must be of type 'numeric'"
+  )
+  expect_error(
+    variant$add_variant(name = "variant", characteristics = list("introduction_date" = "a")),
+    "Must be of class 'Date'"
+  )
+
+  rm(variant)
+})
+
+
+test_that("active binding: variants works", {
+  variant <- DiseasyVariant$new()
+
+  # Retrieve the variants
+  expect_null(variant %.% variants)
+
+  # Try to set the variants
+  # test_that cannot capture this error, so we have to hack it
+  expect_identical(tryCatch(variant$variants <- list("name" = "WT"), error = \(e) e),                                   # nolint: implicit_assignment_linter
+                   simpleError("`$variants` is read only"))
+  expect_null(variant %.% variants)
+
+  rm(variant)
+})
+
+
+test_that("active binding: cross_immunity works", {
+  variant <- DiseasyVariant$new()
+
+  # Retrieve the cross_immunity
+  expect_equal(variant$cross_immunity, matrix(1))
+
+  # Try to set the cross_immunity
+  # test_that cannot capture this error, so we have to hack it
+  expect_identical(tryCatch(variant$cross_immunity <- matrix(2), error = \(e) e),                                       # nolint: implicit_assignment_linter
+                   simpleError("`$cross_immunity` is read only"))
+  expect_equal(variant$cross_immunity, matrix(1))
+
+  rm(variant)
+})
