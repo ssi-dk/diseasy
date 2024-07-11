72ccf546e5d2c4140431e5542f2119cb8b668aee
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-07-11 13:10:41 +0200

    chore(DiseasyModelOdeSeir): Use prefered syntax for `dplyr::summarise`

--- R/DiseasyModelOdeSeir.R
+++ R/DiseasyModelOdeSeir.R
@@ -77,7 +77,7 @@ DiseasyModelOdeSeir <- R6::R6Class(                                             
         proportion <- rep(1 / private %.% n_age_groups, private %.% n_age_groups)
       } else {
         proportion <- self %.% activity %.% map_population(self %.% parameters %.% age_cuts_lower) |>
-          dplyr::summarise("proportion" = sum(.data$proportion), .by = .data$age_group_out) |>
+          dplyr::summarise("proportion" = sum(.data$proportion), .by = "age_group_out") |>
           dplyr::pull("proportion")
       }
 
