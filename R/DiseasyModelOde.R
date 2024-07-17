1b4864b05d702b30dca3d5de8bcd496990d8db00
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-07-17 10:28:42 +0200

    docs: update documentation

--- R/DiseasyModelOde.R
+++ R/DiseasyModelOde.R
@@ -12,9 +12,9 @@ DiseasyModelOde <- R6::R6Class(
     #'   Creates a new instance of the `DiseasyModelOde` [R6][R6::R6Class] class.
     #'   This module is typically not constructed directly but rather through `DiseasyModelOde*` classes,
     #'   such as [DiseasyModelOdeSeir].
+    #' @param training_length `r rd_training_length()`
     #' @param ...
     #'   parameters sent to `DiseasyModel` [R6][R6::R6Class] constructor.
-    #' @details
     initialize = function(training_length, ...) {
 
       coll <- checkmate::makeAssertCollection()
@@ -31,14 +31,14 @@ DiseasyModelOde <- R6::R6Class(
 
     # Roxygen has only limited support for R6 docs currently, so we need to do some tricks for the documentation
     # of get_results
-    #' @description `r .get_results_description`
-    #' @template observable
-    #' @template prediction_length
-    #' @template quantiles
-    #' @template aggregation
-    #' @return `r .get_results_return`
-    #' @seealso `r .get_results_seealso`
-    get_results = function(observable, prediction_length, quantiles = NULL, aggregation = NULL) {
+    #' @description `r rd_get_results_description`
+    #' @param observable `r rd_observable()`
+    #' @param prediction_length `r rd_prediction_length()`
+    #' @param quantiles `r rd_quantiles()`
+    #' @param stratification `r rd_stratification()`
+    #' @return `r rd_get_results_return`
+    #' @seealso `r rd_get_results_seealso`
+    get_results = function(observable, prediction_length, quantiles = NULL, stratification = NULL) {
       coll <- checkmate::makeAssertCollection()
       checkmate::assert_true(!is.null(self %.% observables), add = coll)
       checkmate::assert_choice(observable, self %.% observables %.% available_observables, add = coll)
@@ -49,15 +49,15 @@ DiseasyModelOde <- R6::R6Class(
       hash <- private$get_hash()
       if (!private$is_cached(hash)) {
 
-        # Get the observable at the aggregation level
-        data <- self$get_training_data(observable, aggregation)
+        # Get the observable at the stratification level
+        data <- self$get_training_data(observable, stratification)
 
         # Store in cache
         private$cache(hash, prediction)
       }
 
       # Write to the log
-      private$report_get_results(observable, aggregation, prediction_length, hash)
+      private$report_get_results(observable, stratification, prediction_length, hash)
 
       # Return
       return(private$cache(hash))
