b0b5652cc68e8f59a18c1b82f460b66105566620
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-07-17 10:32:36 +0200

    feat: Add a placeholder for DiseasyVariant

--- R/DiseasyModel.R
+++ R/DiseasyModel.R
@@ -226,7 +226,7 @@ DiseasyModel <- R6::R6Class(                                                    
     ),
 
 
-    #' @field season (`diseasy::season`)\cr
+    #' @field season (`diseasy::DiseasySeason`)\cr
     #'   The local copy of a season module. Read-only.
     #' @seealso [diseasy::DiseasySeason]
     #' @importFrom diseasystore `%.%`
@@ -237,7 +237,7 @@ DiseasyModel <- R6::R6Class(                                                    
     ),
 
 
-    #' @field variant (`diseasy::variant`)\cr
+    #' @field variant (`diseasy::.DiseasyVariant`)\cr
     #'  The local copy of a variant module. Read-only.
     #' @seealso [diseasy::DiseasyVariant]
     #' @importFrom diseasystore `%.%`
