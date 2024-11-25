f17e30e57ebe959690084246d56cc3bf7beeb5c7
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-11-25 13:22:21 +0100

    feat: Add an example data set generated from SEIR model

--- R/data.R
+++ R/data.R
@@ -102,3 +102,29 @@ NULL
 #' @author Lasse Engbo Christiansen \email{lsec@ssi.dk}
 #' @keywords data
 NULL
+
+
+#' example_seir_data
+#'
+#' This data set contains a the simulation output from a SEIR model to use when testing and developing models.
+#'
+#' The underlying model is a SEIR model (`?DiseasyModelOdeSeir`) with 2 exposed compartments, 1 infected compartment
+#' and 1 recovered compartment. The model uses an exposed period of 1 / rE = 2.1 days and a infectious period of
+#' 1 / rI = 4.5 days.
+#' The model uses Denmark for its population and activity scenario with the "baseline" activity unit as the level of
+#' activity. The overall infection risk is set to be 0.025 and initially, 0.2 % of the population is newly exposed
+#' (i.e. placed in the E1 compartments).
+#'
+#' The model outputs included in the dataset is in the form of a tibble with the following columns:
+#'   - `date`: The time for the output.
+#'   - `age_group`: The age group for the output.
+#'   - `n_infected`: The direct output of the SEIR model (rI * I).
+#'   - `n_positive_simple`: A realisation of the number of test-positives with a 65% test probability.
+#'   - `n_positive_simple`: A realisation of the number of test-positives with a overall 65% test probability but with
+#'     a reduced test-probability in the weekend.
+#'
+#' @name example_seir_data
+#' @docType data
+#' @author Rasmus Skytte Randl\\u00F8v \email{rske@dtu.dk}
+#' @keywords data
+NULL
