efc75877ca29440db70604d296552eefdcc6a33f
Rasmus Skytte Randløv <rske@ssi.dk>
2024-12-12 14:49:23 +0100

    feat: Add model-templates for simple regerssion models G0, G1, B0 and B1

--- R/DiseasyModelBRM.R
+++ R/DiseasyModelBRM.R
@@ -95,3 +95,124 @@ DiseasyModelBRM <- R6::R6Class(                                                 
     }
   )
 )
+
+
+#' @title Meta module for the simple, b* reference models models
+#'
+#' @description TODO
+#' @noRd
+DiseasyModelB_ <- R6::R6Class(                                                                                          # nolint: object_name_linter
+  inherit = DiseasyModelBRM,
+  public = list(
+
+    #' @description
+    #'   Creates a new instance of the `DiseasyModelB_` [R6][R6::R6Class] class.
+    #'   This module is typically not constructed directly but rather through `DiseasyModelB*` classes,
+    #'   such as [DiseasyModelB0] and [DiseasyModelB1].
+    #' @param ...
+    #'   parameters sent to `DiseasyModelBRM` [R6][R6::R6Class] constructor.
+    #' @details
+    #'   Helper class for the the `DiseasyModelB*` [R6][R6::R6Class] classes.
+    #' @seealso [stats::family], [stats::as.formula]
+    #' @return
+    #'   A new instance of the `DiseasyModelB_` [R6][R6::R6Class] class.
+    initialize = function(...) {
+      super$initialize(formula = self$formula, family = stats::poisson(), ...)
+    }
+  ),
+
+  private = list(
+    update_formula = function(formula, stratification) {
+
+      # When stratification is given, we treat each group as having their own rates and intercepts
+      if (!is.null(stratification)) {
+
+        # stats::update.formula does not update formulas with only intercept term as expected
+        # with the `*` operator so we need to manually detect if the formula initially is only
+        # intercept and use the `+` operator for the first reduction.
+        initial_operator <- ifelse(rlang::is_empty(labels(terms(formula))), "+", "*")
+
+        # Now we can reduce with the operators set
+        purrr::pmap(
+          tibble::lst(
+            "label" = names(stratification),
+            "stratification" = stratification,
+            "operator" = c(initial_operator, rep("*", length(stratification) - 1))
+          ),
+          \(label, stratification, operator) {
+            glue::glue("~ . {operator} {ifelse(label != '', label, dplyr::as_label(stratification))}") |>
+              stats::as.formula()
+          }
+        ) |>
+          purrr::reduce(stats::update.formula, .init = formula)
+
+      } else { # Do nothing
+        return(formula)
+      }
+    }
+  )
+)
+
+
+#' @title Model module for the b0 reference model
+#' @rdname Diseasy-BRM-models
+#'
+#' @description TODO
+#' @export
+DiseasyModelB0 <- R6::R6Class(                                                                                          # nolint: object_name_linter
+  classname = "DiseasyModelB0",
+  inherit = DiseasyModelB_,
+  public = list(
+    #' @description
+    #'   Creates a new instance of the `DiseasyModelB0` [R6][R6::R6Class] class.
+    #' @param ...
+    #'   parameters sent to `DiseasyModelB_` [R6][R6::R6Class] constructor
+    #' @return
+    #'   A new instance of the `DiseasyModelB0` [R6][R6::R6Class] class.
+    initialize = function(...) {
+      super$initialize(
+        label = "b0",
+        parameters = list(
+          "training_length" = c("training" = 7, "testing" = 0, "validation" = 0)
+        ),
+        ...
+      )
+    }
+  ),
+
+  private = list(
+    .formula = "{observable} ~ 1"   # "{observable}" will be replaced by the observable at runtime
+  )
+)
+
+
+#' @title Model module for the b1 reference model
+#' @rdname Diseasy-BRM-models
+#'
+#' @description TODO
+#' @export
+DiseasyModelB1 <- R6::R6Class(                                                                                          # nolint: object_name_linter
+  classname = "DiseasyModelB1",
+  inherit = DiseasyModelB_,
+  public = list(
+    #' @description
+    #'   Creates a new instance of the `DiseasyModelB1` [R6][R6::R6Class] class.
+    #' @param ...
+    #'   parameters sent to `DiseasyModelB_` [R6][R6::R6Class] constructor
+    #' @return
+    #'   A new instance of the `DiseasyModelB1` [R6][R6::R6Class] class.
+    initialize = function(...) {
+      super$initialize(
+        label = "b1",
+        parameters = list(
+          "training_length" = c("training" = 7, "testing" = 0, "validation" = 0)
+        ),
+        ...
+      )
+    }
+  ),
+
+  private = list(
+    .formula = "{observable} ~ t"   # "{observable}" will be replaced by the observable at runtime
+  )
+)
--- R/DiseasyModelGLM.R
+++ R/DiseasyModelGLM.R
@@ -77,3 +77,122 @@ DiseasyModelGLM <- R6::R6Class(                                                 
     }
   )
 )
+
+
+#' @title Meta module for the simple, g* reference models models
+#'
+#' @description TODO
+#' @noRd
+DiseasyModelG_ <- R6::R6Class(                                                                                          # nolint: object_name_linter
+  inherit = DiseasyModelGLM,
+  public = list(
+
+    #' @description
+    #'   Creates a new instance of the `DiseasyModelG_` [R6][R6::R6Class] class.
+    #'   This module is typically not constructed directly but rather through `DiseasyModelG*` classes,
+    #'   such as [DiseasyModelG0] and [DiseasyModelG1].
+    #' @param ...
+    #'   parameters sent to `DiseasyModelGLM` [R6][R6::R6Class] constructor.
+    #' @details
+    #'   Helper class for the the `DiseasyModelG*` [R6][R6::R6Class] classes.
+    #' @seealso [stats::family], [stats::as.formula]
+    #' @return
+    #'   A new instance of the `DiseasyModelG_` [R6][R6::R6Class] class.
+    initialize = function(...) {
+      super$initialize(formula = self$formula,
+                       family = stats::quasipoisson(),
+                       ...)
+    }
+  ),
+
+  private = list(
+    update_formula = function(formula, aggregation) {
+
+      # When aggregation is given, we treat each group as having their own rates and intercepts
+      if (!is.null(aggregation)) {
+
+        # stats::update.formula does not update formulas with only intercept term as expected
+        # when using the `*` operator so we need to manually detect if the formula initially is only
+        # intercept and use the `+` operator for the first reduction.
+        initial_operator <- ifelse(rlang::is_empty(labels(terms(formula))), "+", "*")
+
+        # Now we can reduce with the operators set
+        purrr::pmap(tibble::lst(label = names(aggregation),
+                                aggregation = aggregation,
+                                operator = c(initial_operator, rep("*", length(label) - 1))),
+                    \(label, aggregation, operator) {
+                      glue::glue("~ . {operator} {ifelse(label != '', label, dplyr::as_label(aggregation))}") |>
+                        stats::as.formula()
+                    }) |>
+          purrr::reduce(stats::update.formula, .init = formula)
+
+      } else { # Do nothing
+        return(formula)
+      }
+    }
+  )
+)
+
+
+#' @title Model module for the g0 reference model
+#' @rdname Diseasy-GLM-models
+#'
+#' @description TODO
+#' @export
+DiseasyModelG0 <- R6::R6Class(                                                                                          # nolint: object_name_linter
+  classname = "DiseasyModelG0",
+  inherit = DiseasyModelG_,
+  public = list(
+    #' @description
+    #'   Creates a new instance of the `DiseasyModelG0` [R6][R6::R6Class] class.
+    #' @param ...
+    #'   parameters sent to `DiseasyModelG_` [R6][R6::R6Class] constructor
+    #' @return
+    #'   A new instance of the `DiseasyModelG1` [R6][R6::R6Class] class.
+    initialize = function(...) {
+      super$initialize(
+        label = "g0",
+        parameters = list(
+          "training_length" = c("training" = 7, "testing" = 0, "validation" = 0)
+        ),
+        ...
+      )
+    }
+  ),
+
+  private = list(
+    .formula = "{observable} ~ 1"   # "{observable}" will be replaced by the observable at runtime
+  )
+)
+
+
+#' @title Model module for the g1 reference model
+#' @rdname Diseasy-GLM-models
+#'
+#' @description TODO
+#' @export
+DiseasyModelG1 <- R6::R6Class(                                                                                          # nolint: object_name_linter
+  classname = "DiseasyModelG1",
+  inherit = DiseasyModelG_,
+  public = list(
+    #' @description
+    #'   Creates a new instance of the `DiseasyModelG1` [R6][R6::R6Class] class.
+    #' @param ...
+    #'   parameters sent to `DiseasyModelG_` [R6][R6::R6Class] constructor
+    #' @return
+    #'   A new instance of the `DiseasyModelG1` [R6][R6::R6Class] class.
+    initialize = function(...) {
+      super$initialize(
+        label = "g1",
+        parameters = list(
+          "training_length" = c("training" = 21, "testing" = 0, "validation" = 0)
+        ),
+        ...
+      )
+    }
+  ),
+
+  private = list(
+    .formula = "{observable} ~ t"   # "{observable}" will be replaced by the observable at runtime
+  )
+)
