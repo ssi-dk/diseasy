e7fbf2a28ea0c6aea93e46d901eb5e14b1f8d857
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2024-12-16 13:27:30 +0100

    test(hypoexponential): Add Monte Carlo tests for the hypoexponential distribution

--- tests/testthat/test-0_hypoexponential.R
+++ tests/testthat/test-0_hypoexponential.R
@@ -1,26 +1,91 @@
-# Generate known values for comparison
-known_values <- data.frame(
-  t = seq(from = 0, to = 10, by = 0.5),
-  dhypo = c(
-    1.0000000, 0.6065307, 0.3678794, 0.2231302, 0.1353353, 0.0820850, 0.0497871, 0.0301974, 0.0183156, 0.0111090,
-    0.0067379, 0.0040868, 0.0024788, 0.0015034, 0.0009119, 0.0005531, 0.0003355, 0.0002035, 0.0001234, 0.0000745,
-    0.0000455
-  ),
-  phypo = c(
-    0.0000000, 0.3934693, 0.6321206, 0.7768698, 0.8646647, 0.9179150, 0.9502129, 0.9698026, 0.9816844, 0.9888910,
-    0.9932621, 0.9959132, 0.9975212, 0.9984966, 0.9990881, 0.9994469, 0.9996645, 0.9997965, 0.9998766, 0.9999255,
-    0.9999546
+# In one limit, the hypoexponential is just the Erlang-distribution, so we check this limits works
+test_that("dhypo matches Erlang distribution", {
+  expect_equal(
+    dhypo(seq(from = 0, to = 5, length.out = 5), shape = 1, rate = 1),
+    stats::dgamma(seq(from = 0, to = 5, length.out = 5), shape = 1, rate = 1)
   )
+})
+
+test_that("phypo matches Erlang distribution", {
+  expect_equal(
+    phypo(seq(from = 0, to = 5, length.out = 5), shape = 1, rate = 1),
+    stats::pgamma(seq(from = 0, to = 5, length.out = 5), shape = 1, rate = 1)
+  )
+})
+
+test_that("qhypo matches Erlang distribution", {
+  expect_equal(
+    qhypo(seq(from = 0.05, to = 0.95, length.out = 5), shape = 1, rate = 1),
+    stats::qgamma(seq(from = 0.05, to = 0.95, length.out = 5), shape = 1, rate = 1)
+  )
+})
+
+
+# For the non-edge-case, we compare numerically with different Poisson processes and check that they are close
+test_rates <- list(                                                                                                     # nolint start: object_name_linter
+  "erlang case 1" = \(shape) 1,
+  "erlang case 2" = \(shape) rep(1, shape),
+  "increasing rates" = \(shape) 1 / seq(from = 1, to = 2, length.out = shape),
+  "decreasing rates" = \(shape) 1 / seq(from = 2, to = 1, length.out = shape)
 )
 
-test_that("dhypo matches known values", {
-  expect_equal(dhypo(known_values$t, shape = 1, rate = 1), known_values$dhypo, tolerance = 1e-6)
-})
+n_samples <- 1e6 # Number of Monte Carlo samples in the test
 
-test_that("phypo matches known values", {
-  expect_equal(phypo(known_values$t, shape = 1, rate = 1), known_values$phypo, tolerance = 1e-6)
-})
+# Test each scenario
+for (shape in c(1, 5)) { # Number of compartments
+  purrr::imap(test_rates, \(rates, rate_name) {                                                                             # nolint: object_name_linter
 
-test_that("qhypo matches known values", {
-  expect_equal(qhypo(known_values$phypo, shape = 1, rate = 1), known_values$t, tolerance = 1e-3) # uniroot bad?
-})
+    test_that(glue::glue("`dhypo()` works for shape = {shape} and {rate_name} rates"), {
+
+      # The Monte Carlo (mc) rates needs to be of length shape
+      r <- rates(shape)
+      if (length(r) == 1) r <- rep(r, shape)
+
+      # Generate samples from the corresponding hypoexponential distribution
+      set.seed(42)
+      rhypo_samples <- r |>
+        purrr::map(~ stats::rexp(n_samples, rate = .)) |>
+        purrr::reduce(`+`)
+
+      # Convert samples to cumulated density
+      t <- seq(from = 0, to = 5 * sum(1 / r), length.out = 20)
+      hypo_ecdf <- stats::ecdf(rhypo_samples)(t)
+
+      # Compute CDF from dhypo and check if close to the Monte Carlo results
+      expect_equal(
+        purrr::map_dbl(
+          t,
+          \(tp) {
+            stats::integrate(
+              f = \(t) dhypo(t, shape = shape, rate = r),
+              lower = 0,
+              upper = tp,
+              subdivisions = 1e3,
+              rel.tol = 1e-12
+            )$value
+          }
+        ),
+        hypo_ecdf,
+        tolerance = 1e-3
+      )
+
+
+      # Compute phypo and check if close to the Monte Carlo results
+      expect_equal(
+        purrr::map_dbl(t, \(t) phypo(t, shape = shape, rate = r)),
+        hypo_ecdf,
+        tolerance = 1e-3
+      )
+
+      # For the smaller quantiles in the Monte Carlo data, check if qhypo is close
+      expect_equal(
+        purrr::map_dbl(
+          purrr::keep(hypo_ecdf, ~ . < 0.95),
+          \(q) qhypo(q, shape = shape, rate = r)
+        ),
+        t[hypo_ecdf < 0.95],
+        tolerance = 1e-3
+      )
+    })
+  })
+}
