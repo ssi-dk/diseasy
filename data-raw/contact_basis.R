e93fbfac94bd38b1bc7ce02c316d8f4596a9e1bb
Rasmus Skytte Randløv <rske@ssi.dk>
2026-05-28 20:41:27 +0000

    refactor(contact_basis_and_demography): Split in two files

--- data-raw/contact_basis.R
+++ data-raw/contact_basis.R
@@ -5,26 +5,6 @@ if (rlang::is_installed(c("contactdata", "countrycode", "curl", "usethis", "tibb
   age_cuts <- (0:15) * 5
   age_labels <- diseasystore::age_labels(age_cuts)
 
-  # We get age population data like the contactdata package does using this source:
-  # https://www.census.gov/programs-surveys/international-programs/about/idb.html
-  # Terms of the data can be read here:
-  # https://www.census.gov/data/developers/about/terms-of-service.html
-
-  # Alternative data source is here:
-  # https://population.un.org/wpp/Download/Standard/Population/
-
-  # US Census data uses their "GEO_ID" as geographical identifier. In this case, we only need the
-  # country code (last two characters of GEO_ID)
-  idb_zip <- "https://www.census.gov/data-tools/demo/data/idb/dataset/idbzip.zip"
-  curl::curl_fetch_disk(idb_zip, file.path(tempdir(), "idb.zip"))
-  idb_1yr <- readr::read_delim(
-    unz(file.path(tempdir(), "idb.zip"), "idbsingleyear.txt"),
-    delim = "|",
-    show_col_types = FALSE
-  ) |>
-    dplyr::mutate("key_country" = stringr::str_sub(.data$GEO_ID, -2, -1))
-
-
   # Get countries in the contactdata package
   countries <- contactdata::list_countries()
   country_codes <- purrr::map_chr(
@@ -35,27 +15,7 @@ if (rlang::is_installed(c("contactdata", "countrycode", "curl", "usethis", "tibb
 
   # Export contact matrices where we also have population data
   common_country_codes <- country_codes |>
-    purrr::keep(~ . %in% unique(idb_1yr$key_country))
-
-  # Get 1-year age-group data for all countries in the data set
-  # We use population data from 2020 to match the study year of `contactdata`s contact matrices
-  demography <- idb_1yr |>
-    dplyr::rename_with(tolower) |>
-    dplyr::filter(
-      `#yr` == 2020,
-      .data$sex == 0
-    ) |>
-    dplyr::transmute(
-      .data$key_country,
-      .data$age,
-      "n_population" = .data$pop
-    )
-
-  attr(demography, "description") <- "Population data from the US Census Bureau for 2020."
-
-  # Store the data in the package
-  attr(demography, "creation_datetime") <- Sys.time()
-  usethis::use_data(demography, overwrite = TRUE)
+    purrr::keep(~ . %in% unique(demography$key_country))
 
 
   # Get all individual matrices for each country we export
