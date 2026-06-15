c54d1fdd46a3c75d3e2c6456a1bf8ec2bfb7a817
Rasmus Skytte Randløv <rasmusskytteeriksen@gmail.com>
2026-06-15 13:58:23 +0200

    feat(demography_nuts): Create new nordic subset

--- R/data.R
+++ R/data.R
@@ -98,11 +98,11 @@ NULL
 #'  - `sex`: The sex.
 #'  - `population`: The population count.
 #'
-#' @name demography_nuts3
+#' @name demography_nordic_nuts3
 #' @docType data
 #' @author Rasmus Skytte Randl&oslash;v \email{rske@ssi.dk}
 #' @source
-#'   Source: 10.2908/demo_r_pjangrp3, `r format(as.Date(attr(demography_nuts3, "creation_datetime")), "%B %d, %Y")`.
+#'   Source: 10.2908/demo_r_pjangrp3, `r format(as.Date(attr(demography_nordic_nuts3, "creation_datetime")), "%B %d, %Y")`.
 #'   Population on 1 January by age group, sex and NUTS 3 region
 #' @keywords data
 NULL
@@ -122,7 +122,7 @@ NULL
 #' @docType data
 #' @author Rasmus Skytte Randl&oslash;v \email{rske@ssi.dk}
 #' @source
-#'   Source: 10.2908/demo_r_pjangrp3, `r format(as.Date(attr(demography_nuts3, "creation_datetime")), "%B %d, %Y")`.
+#'   Source: 10.2908/demo_r_pjangrp3, `r format(as.Date(attr(nuts, "creation_datetime")), "%B %d, %Y")`.
 #'   Population on 1 January by age group, sex and NUTS 3 region
 #' @keywords data
 NULL
---
+++ data-raw/demography_nordic_nuts3.R
@@ -0,0 +1,10 @@
+output <- generate_demography_nuts3(
+  regions = c("DK", "FI", "IS", "NO", "SE"),
+  output_nuts = TRUE
+)
+
+demography_nordic_nuts3 <- output %.% demography_nuts3
+usethis::use_data(demography_nordic_nuts3, overwrite = TRUE)
+
+nuts <- output %.% nuts
+usethis::use_data(nuts, overwrite = TRUE)
