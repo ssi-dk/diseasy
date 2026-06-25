f963bc93a44ffc476dd996333541244a9c2178db
Rasmus Skytte Randløv <rske@ssi.dk>
2026-06-25 03:46:15 +0000

    Revert "test(DiseasyModelOdeSeir-configurable-observables): Increase test tolerances further"
    This reverts commit eda37f19e2585695c4a3c7fddcdb75702f124c89.

--- tests/testthat/test-DiseasyModelOdeSeir-configurable-observables.R
+++ tests/testthat/test-DiseasyModelOdeSeir-configurable-observables.R
@@ -152,7 +152,7 @@ tidyr::expand_grid(
       reference_after <- m$get_results("n_infected", prediction_length = 10)$n_infected
 
       # These should be very close to identical
-      expect_equal(reference_before, reference_after, tolerance = 5e-3) # within 5 per mille
+      expect_equal(reference_before, reference_after, tolerance = 1e-3) # within 1 per mille
 
       # For our other observables, we expect some difference:
       # 1) For the state_vector observable, we should have little difference since we
