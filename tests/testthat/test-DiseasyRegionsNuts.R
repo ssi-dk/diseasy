dc0fa0b0506bb2331f06a4466224af47a1eae4f9
Rasmus Skytte Randløv <rske@ssi.dk>
2026-07-07 10:30:53 +0000

    feat(DiseasyRegions): Disambiguate "regions" = "DiseasyRegions" and "regions" = "Area of interest"

--- tests/testthat/test-DiseasyRegionsNuts.R
+++ tests/testthat/test-DiseasyRegionsNuts.R
@@ -38,52 +38,52 @@ test_that("Empty initialize works", {
 })
 
 
-test_that("`$set_regions()`` works", {
+test_that("`$set_area()`` works", {
 
-  region_1 <- DiseasyRegionsNuts$new()
-  region_1$set_regions(c("DK01", "DK02"))
+  regions_nuts_1 <- DiseasyRegionsNuts$new()
+  regions_nuts_1$set_area(c("DK01", "DK02"))
 
-  region_2 <- DiseasyRegionsNuts$new()
-  region_2$set_regions(c("DK02", "DK01"))
+  regions_nuts_2 <- DiseasyRegionsNuts$new()
+  regions_nuts_2$set_area(c("DK02", "DK01"))
 
-  expect_identical(region_1 %.% regions, region_2 %.% regions)
-  expect_identical(region_1 %.% hash, region_2 %.% hash)
+  expect_identical(regions_nuts_1 %.% area, regions_nuts_2 %.% area)
+  expect_identical(regions_nuts_1 %.% hash, regions_nuts_2 %.% hash)
 
-  rm(region_1)
-  rm(region_2)
+  rm(regions_nuts_1)
+  rm(regions_nuts_2)
 })
 
 
 test_that("`$set_adjacency()`` works", {
 
-  region_1 <- DiseasyRegionsNuts$new()
-  region_1$set_adjacency(test_adjacency)
+  regions_nuts_1 <- DiseasyRegionsNuts$new()
+  regions_nuts_1$set_adjacency(test_adjacency)
 
-  region_2 <- DiseasyRegionsNuts$new()
-  region_2$set_adjacency(test_adjacency[sample(nrow(test_adjacency)), ])
+  regions_nuts_2 <- DiseasyRegionsNuts$new()
+  regions_nuts_2$set_adjacency(test_adjacency[sample(nrow(test_adjacency)), ])
 
-  expect_identical(region_1 %.% adjacency, region_2 %.% adjacency)
-  expect_identical(region_1 %.% infection_flow_matrix, region_2 %.% infection_flow_matrix)
-  expect_identical(region_1 %.% hash, region_2 %.% hash)
+  expect_identical(regions_nuts_1 %.% adjacency, regions_nuts_2 %.% adjacency)
+  expect_identical(regions_nuts_1 %.% infection_flow_matrix, regions_nuts_2 %.% infection_flow_matrix)
+  expect_identical(regions_nuts_1 %.% hash, regions_nuts_2 %.% hash)
 
-  rm(region_1)
-  rm(region_2)
+  rm(regions_nuts_1)
+  rm(regions_nuts_2)
 })
 
 
 test_that("`$set_demography()`` works", {
 
-  region_1 <- DiseasyRegionsNuts$new()
-  region_1$set_demography(demography_nordic_nuts3)
+  regions_nuts_1 <- DiseasyRegionsNuts$new()
+  regions_nuts_1$set_demography(demography_nordic_nuts3)
 
-  region_2 <- DiseasyRegionsNuts$new()
-  region_2$set_demography(demography_nordic_nuts3[sample(nrow(demography_nordic_nuts3)), ])
+  regions_nuts_2 <- DiseasyRegionsNuts$new()
+  regions_nuts_2$set_demography(demography_nordic_nuts3[sample(nrow(demography_nordic_nuts3)), ])
 
-  expect_identical(region_1 %.% demography, region_2 %.% demography)
-  expect_identical(region_1 %.% hash, region_2 %.% hash)
+  expect_identical(regions_nuts_1 %.% demography, regions_nuts_2 %.% demography)
+  expect_identical(regions_nuts_1 %.% hash, regions_nuts_2 %.% hash)
 
-  rm(region_1)
-  rm(region_2)
+  rm(regions_nuts_1)
+  rm(regions_nuts_2)
 })
 
 
@@ -92,7 +92,7 @@ test_that("Malformed inputs to initialize works", {
   expect_error(
     checkmate_err_msg(
       DiseasyRegionsNuts$new(
-        regions = "IS001",
+        area = "IS001",
         adjacency = dplyr::filter(test_adjacency, .data$from != "IS001", .data$to != "IS001")
       )
     ),
@@ -103,7 +103,7 @@ test_that("Malformed inputs to initialize works", {
   expect_error(
     checkmate_err_msg(
       DiseasyRegionsNuts$new(
-        regions = "IS001",
+        area = "IS001",
         adjacency = test_adjacency,
         demography = dplyr::filter(demography_nordic_nuts3, .data$region != "IS001")
       )
@@ -117,24 +117,24 @@ test_that("Malformed inputs to initialize works", {
 
 test_that("Non-empty initialize works", {
 
-  region <- DiseasyRegionsNuts$new(
-    regions = "IS001",
+  regions_nuts <- DiseasyRegionsNuts$new(
+    area = "IS001",
     adjacency = test_adjacency,
     demography = demography_nordic_nuts3
   )
 
-  expect_identical(region %.% regions, "IS001")
-  expect_identical(unique(region %.% demography %.% region), "IS001")
+  expect_identical(regions_nuts %.% area, "IS001")
+  expect_identical(unique(regions_nuts %.% demography %.% region), "IS001")
   expect_identical(
-    sum(region %.% demography %.% population),
+    sum(regions_nuts %.% demography %.% population),
     sum(dplyr::pull(dplyr::filter(demography_nordic_nuts3, .data$region == "IS001"), "population"))
   )
 
-  expect_identical(nrow(region %.% adjacency), 1L)
-  expect_identical(region %.% adjacency %.% from, "IS001")
-  expect_identical(region %.% adjacency %.% to, "IS001")
+  expect_identical(nrow(regions_nuts %.% adjacency), 1L)
+  expect_identical(regions_nuts %.% adjacency %.% from, "IS001")
+  expect_identical(regions_nuts %.% adjacency %.% to, "IS001")
 
-  rm(region)
+  rm(regions_nuts)
 })
 
 
@@ -142,39 +142,39 @@ test_that("Non-empty initialize works", {
 test_that("Setters are commutative", {
 
   # Permutaiton 1
-  region <- DiseasyRegionsNuts$new()
-  region$set_regions(c("IS001", "IS002"))
-  region$set_adjacency(test_adjacency)
-  region$set_demography(demography_nordic_nuts3)
-  hash_1 <- region$hash
-  rm(region)
+  regions_nuts <- DiseasyRegionsNuts$new()
+  regions_nuts$set_area(c("IS001", "IS002"))
+  regions_nuts$set_adjacency(test_adjacency)
+  regions_nuts$set_demography(demography_nordic_nuts3)
+  hash_1 <- regions_nuts$hash
+  rm(regions_nuts)
 
 
   # Permutation 2
-  region <- DiseasyRegionsNuts$new()
-  region$set_adjacency(test_adjacency)
-  region$set_regions(c("IS001", "IS002"))
-  region$set_demography(demography_nordic_nuts3)
-  hash_2 <- region$hash
-  rm(region)
+  regions_nuts <- DiseasyRegionsNuts$new()
+  regions_nuts$set_adjacency(test_adjacency)
+  regions_nuts$set_area(c("IS001", "IS002"))
+  regions_nuts$set_demography(demography_nordic_nuts3)
+  hash_2 <- regions_nuts$hash
+  rm(regions_nuts)
 
 
   # Permutation 3
-  region <- DiseasyRegionsNuts$new()
-  region$set_adjacency(test_adjacency)
-  region$set_demography(demography_nordic_nuts3)
-  region$set_regions(c("IS001", "IS002"))
-  hash_3 <- region$hash
-  rm(region)
+  regions_nuts <- DiseasyRegionsNuts$new()
+  regions_nuts$set_adjacency(test_adjacency)
+  regions_nuts$set_demography(demography_nordic_nuts3)
+  regions_nuts$set_area(c("IS001", "IS002"))
+  hash_3 <- regions_nuts$hash
+  rm(regions_nuts)
 
 
   # Permutation 4
-  region <- DiseasyRegionsNuts$new()
-  region$set_demography(demography_nordic_nuts3)
-  region$set_adjacency(test_adjacency)
-  region$set_regions(c("IS001", "IS002"))
-  hash_4 <- region$hash
-  rm(region)
+  regions_nuts <- DiseasyRegionsNuts$new()
+  regions_nuts$set_demography(demography_nordic_nuts3)
+  regions_nuts$set_adjacency(test_adjacency)
+  regions_nuts$set_area(c("IS001", "IS002"))
+  hash_4 <- regions_nuts$hash
+  rm(regions_nuts)
 
   expect_length(
     unique(c(hash_1, hash_2, hash_3, hash_4)),
@@ -185,131 +185,131 @@ test_that("Setters are commutative", {
 
 test_that("$region_filter() works", {
 
-  region <- DiseasyRegionsNuts$new(
-    regions = "IS001",
+  regions_nuts <- DiseasyRegionsNuts$new(
+    area = "IS001",
     adjacency = test_adjacency,
     demography = demography_nordic_nuts3
   )
 
   expect_identical(
-    region$region_filter(values = c("IS001", "IS002"), regions = "IS001"),
+    regions_nuts$region_filter(values = c("IS001", "IS002"), area = "IS001"),
     c(TRUE, FALSE)
   )
   expect_identical(
-    region$region_filter(values = c("IS001", "IS002"), regions = NULL),
+    regions_nuts$region_filter(values = c("IS001", "IS002"), area = NULL),
     c(TRUE, TRUE)
   )
 
-  rm(region)
+  rm(regions_nuts)
 })
 
 
 test_that("region filtering works", {
 
-  region <- DiseasyRegionsNuts$new(
-    regions = "IS001",
+  regions_nuts <- DiseasyRegionsNuts$new(
+    area = "IS001",
     adjacency = test_adjacency,
     demography = demography_nordic_nuts3
   )
 
-  checkmate::expect_subset(region %.% demography %.% region, "IS001")
-  checkmate::expect_subset(region %.% adjacency %.% from, "IS001")
-  checkmate::expect_subset(region %.% adjacency %.% to, "IS001")
+  checkmate::expect_subset(regions_nuts %.% demography %.% region, "IS001")
+  checkmate::expect_subset(regions_nuts %.% adjacency %.% from, "IS001")
+  checkmate::expect_subset(regions_nuts %.% adjacency %.% to, "IS001")
 
-  region$set_regions(c("IS001", "IS002"))
-  checkmate::expect_subset(region %.% demography %.% region, c("IS001", "IS002"))
-  checkmate::expect_subset(region %.% adjacency %.% from, c("IS001", "IS002"))
-  checkmate::expect_subset(region %.% adjacency %.% to, c("IS001", "IS002"))
+  regions_nuts$set_area(c("IS001", "IS002"))
+  checkmate::expect_subset(regions_nuts %.% demography %.% region, c("IS001", "IS002"))
+  checkmate::expect_subset(regions_nuts %.% adjacency %.% from, c("IS001", "IS002"))
+  checkmate::expect_subset(regions_nuts %.% adjacency %.% to, c("IS001", "IS002"))
 
-  rm(region)
+  rm(regions_nuts)
 })
 
 
 test_that("adjacency matrix normalisation works", {
 
-  region_1 <- DiseasyRegionsNuts$new(
-    regions = c("IS001", "IS002"),
+  regions_nuts_1 <- DiseasyRegionsNuts$new(
+    area = c("IS001", "IS002"),
     adjacency = test_adjacency,
     demography = demography_nordic_nuts3
   )
 
-  region_2 <- DiseasyRegionsNuts$new(
-    regions = c("IS001", "IS002"),
+  regions_nuts_2 <- DiseasyRegionsNuts$new(
+    area = c("IS001", "IS002"),
     adjacency = dplyr::mutate(test_adjacency, "adjacency" = 2 * .data$adjacency),
     demography = demography_nordic_nuts3
   )
 
   expect_equal(                                                                                                         # nolint: expect_identical_linter
-    region_1$infection_flow_matrix,
-    region_2$infection_flow_matrix,
+    regions_nuts_1$infection_flow_matrix,
+    regions_nuts_2$infection_flow_matrix,
     tolerance = 1e-10
   )
 
 
-  rm(region_1)
-  rm(region_2)
+  rm(regions_nuts_1)
+  rm(regions_nuts_2)
 })
 
 
 test_that("`demography` data set works with `DiseasyRegionsNuts`", {
 
-  region <- DiseasyRegionsNuts$new()
-  expect_no_error(region$set_demography(demography_nordic))
-  expect_no_error(region$set_regions(c("DK", "SE")))
+  regions_nuts <- DiseasyRegionsNuts$new()
+  expect_no_error(regions_nuts$set_demography(demography_nordic))
+  expect_no_error(regions_nuts$set_area(c("DK", "SE")))
 
-  rm(region)
+  rm(regions_nuts)
 })
 
 
 test_that("`demography_nordic_nuts3` data set works with `DiseasyRegionsNuts`", {
 
-  region <- DiseasyRegionsNuts$new()
-  expect_no_error(region$set_demography(demography_nordic_nuts3))
-  expect_no_error(region$set_regions(c("DK011", "DK012")))
+  regions_nuts <- DiseasyRegionsNuts$new()
+  expect_no_error(regions_nuts$set_demography(demography_nordic_nuts3))
+  expect_no_error(regions_nuts$set_area(c("DK011", "DK012")))
 
-  rm(region)
+  rm(regions_nuts)
 })
 
 test_that("Hierarchical regions works", {
 
-  region_0 <- DiseasyRegionsNuts$new(
-    regions = "IS",
+  regions_nuts_0 <- DiseasyRegionsNuts$new(
+    area = "IS",
     adjacency = test_adjacency,
     demography = demography_nordic_nuts3
   )
 
-  region_1 <- DiseasyRegionsNuts$new(
-    regions = "IS0",
+  regions_nuts_1 <- DiseasyRegionsNuts$new(
+    area = "IS0",
     adjacency = test_adjacency,
     demography = demography_nordic_nuts3
   )
 
-  region_2 <- DiseasyRegionsNuts$new(
-    regions = "IS00",
+  regions_nuts_2 <- DiseasyRegionsNuts$new(
+    area = "IS00",
     adjacency = test_adjacency,
     demography = demography_nordic_nuts3
   )
 
   # For Malta, NUTS "0", NUTS 1 and NUTS 2 are the same
   expect_identical(
-    region_0$demography,
-    region_1$demography
+    regions_nuts_0$demography,
+    regions_nuts_1$demography
   )
 
   expect_identical(
-    region_0$demography,
-    region_2$demography
+    regions_nuts_0$demography,
+    regions_nuts_2$demography
   )
 
-  rm(region_0)
-  rm(region_1)
-  rm(region_2)
+  rm(regions_nuts_0)
+  rm(regions_nuts_1)
+  rm(regions_nuts_2)
 })
 
 test_that("$regions_at_stratification() tries to guess regions even if `regions` are not configured", {
 
   # Only demography
-  region <- DiseasyRegionsNuts$new(
+  regions_nuts <- DiseasyRegionsNuts$new(
     demography = demography_nordic_nuts3
   )
 
@@ -322,21 +322,21 @@ test_that("$regions_at_stratification() tries to guess regions even if `regions`
     unique() |>
     sort()
 
-  expect_null(region %.% regions)
+  expect_null(regions_nuts %.% area)
 
   expect_identical(
-    region$regions_at_stratification(regional_stratification = "NUTS 0"),
+    regions_nuts$regions_at_stratification(regional_stratification = "NUTS 0"),
     expected_nuts0
   )
 
   expect_identical(
-    region$regions_at_stratification(regional_stratification = "NUTS 3"),
+    regions_nuts$regions_at_stratification(regional_stratification = "NUTS 3"),
     expected_nuts3
   )
 
 
   # Intersection of demography and adjacency
-  region$set_adjacency(
+  regions_nuts$set_adjacency(
     dplyr::filter(
       adjacency_meta_nordic_nuts,
       substr(.data$from, 1, 2) %in% c("DK", "SE"),
@@ -345,7 +345,7 @@ test_that("$regions_at_stratification() tries to guess regions even if `regions`
   )
 
   expect_identical(
-    region$regions_at_stratification(regional_stratification = "NUTS 0"),
+    regions_nuts$regions_at_stratification(regional_stratification = "NUTS 0"),
     c("DK", "SE")
   )
 
@@ -356,16 +356,16 @@ test_that("$regions_at_stratification() tries to guess regions even if `regions`
     sort()
 
   expect_identical(
-    region$regions_at_stratification(regional_stratification = "NUTS 3"),
+    regions_nuts$regions_at_stratification(regional_stratification = "NUTS 3"),
     expected_nuts3
   )
 
 
   # With regions defined
-  region$set_regions("DK")
+  regions_nuts$set_area("DK")
 
   expect_identical(
-    region$regions_at_stratification(regional_stratification = "NUTS 0"),
+    regions_nuts$regions_at_stratification(regional_stratification = "NUTS 0"),
     "DK"
   )
 
@@ -376,9 +376,9 @@ test_that("$regions_at_stratification() tries to guess regions even if `regions`
     sort()
 
   expect_identical(
-    region$regions_at_stratification(regional_stratification = "NUTS 3"),
+    regions_nuts$regions_at_stratification(regional_stratification = "NUTS 3"),
     expected_nuts3
   )
 
-  rm(region)
+  rm(regions_nuts)
 })
