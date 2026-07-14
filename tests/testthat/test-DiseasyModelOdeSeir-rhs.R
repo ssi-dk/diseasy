6ebc11011d3ec7935a9f23820d2912a6946c7d1e
Rasmus Skytte Randløv <rske@ssi.dk>
2026-07-14 22:53:20 +0200

    Update test-DiseasyModelOdeSeir-rhs.R

--- tests/testthat/test-DiseasyModelOdeSeir-rhs.R
+++ tests/testthat/test-DiseasyModelOdeSeir-rhs.R
@@ -611,7 +611,7 @@ test_that("RHS sanity check 5: Activity changes (double variant / double age gro
       conn = DBI::dbConnect(RSQLite::SQLite()),
       last_queryable_date = Sys.Date() - 1
     ),
-    population = DiseasyPopulation$new(age_cuts_lower = c(0, 50)),
+    population = DiseasyPopulation$new(age_cuts_lower = c(0, 40)),
     variant = var,
     parameters = list(
       "compartment_structure" = c("E" = 1L, "I" = 1L, "R" = 1L),
