72c35b4b47e453b1b334e2bf7a4cdf3513d22b45
Rasmus Skytte Randløv <rske@ssi.dk>
2026-08-06 11:21:30 +0000

    chore(diseasy-package): Manually import R-CMD-Check blindspot functions

--- R/diseasy-package.R
+++ R/diseasy-package.R
@@ -4,3 +4,11 @@
 #' @importFrom diseasystore %.%
 #' @importFrom rlang .data
 NULL
+
+# R CMD Check cannot see use of all imports within R6 so we need to manually flag them
+#' @importFrom cachem cache_disk
+#' @importFrom lgr without_logging
+#' @importFrom Matrix Matrix
+#' @importFrom pracma logseq
+#' @importFrom R6 R6Class
+NULL
